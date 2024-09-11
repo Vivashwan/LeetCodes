24234e43e7361584d0c55d598531a922885cef0a
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2024-09-11 17:29:54 +0530

    Time: 2 ms (92.39%) | Memory: 10.5 MB (65.64%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -3,31 +3,34 @@ public:
     vector<int> minOperations(string boxes) {
         int n = boxes.size();
 
-        vector<int>onesIndices;
-        
-        for(int i=0; i<n; i++)
+        vector<int>pref(n, 0);
+
+        int ones = boxes[0] == '1';
+
+        for(int i=1; i<n; i++)
         {
+            pref[i] = pref[i-1] + ones;
+
             if(boxes[i]=='1')
             {
-                onesIndices.push_back(i);
+                ones++;
             }
-        }   
-
-        int m = onesIndices.size();
-
-        vector<int>res;
-
-        for(int i=0; i<n; i++)
-        {
-            int val = 0;
-            for(int j=0; j<m; j++)
-            {
-                val+=abs(onesIndices[j]-i);
-            }
-
-            res.push_back(val);
         }
 
-        return res;
+        ones = 0;
+        int prev = 0;
+
+        for(int i=n-1; i>=0; i--)
+        {
+            pref[i] = pref[i]+prev;
+
+            if(boxes[i]=='1')
+            {
+                ones++;
+            }
+            prev+=ones;
+        }
+
+        return pref;
     }
 };
