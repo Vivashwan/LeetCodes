d6472f6739720110d670965c2dd029215607c261
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2024-11-24 10:48:21 +0530

    Time: 97 ms (41.48%) | Memory: 102.9 MB (90.92%) - LeetSync

--- 2118-maximum-earnings-from-taxi/maximum-earnings-from-taxi.cpp
+++ 2118-maximum-earnings-from-taxi/maximum-earnings-from-taxi.cpp
@@ -1,50 +1,29 @@
+long long dp[30001];
 class Solution {
-private:
-    long long func(int curr, unordered_map<long long, vector<pair<long long, long long>>>&mp,  vector<long long>&dp, int &n)
-    {
-        if(curr==n)
-        {
-            return 0;
-        }
-
-        if(dp[curr]!=-1)
-        {
-            return dp[curr];
-        }
-
-        if(mp[curr].size()>0)
-        {
-            long long pick = INT_MIN;
-
-            for(int i=0; i<mp[curr].size(); i++)
-            {
-                auto p = mp[curr][i];
-
-                long long start = curr, end = p.first, tip = p.second;
-
-                long long temp = (end-start+tip) + func(end, mp, dp, n);
-
-                pick = max(pick, temp);
-            }
-
-            long long notPick = 0 + func(curr+1, mp, dp, n);
-            return dp[curr] = max(notPick, pick);
-        }
-        else return dp[curr] = 0 + func(curr+1, mp, dp, n);
-
-        return 0;
-    }
 public:
-    long long maxTaxiEarnings(int n, vector<vector<int>>& rides) {
-        unordered_map<long long, vector<pair<long long, long long>>>mp;
-
-        vector<long long>dp(n+1, -1);
-
-        for(auto it: rides)
-        {
-            mp[it[0]].push_back({it[1], it[2]});
-        }
-
-        return func(1, mp, dp, n);
+long long help(vector<vector<int>> &ar,int pos){
+    int n=ar.size();
+    if(pos==n) return 0;
+    if(dp[pos]!=-1) return dp[pos];
+    // either I attend ar[pos] trip or not
+    long long ans=help(ar,pos+1);
+    // find the first starting time JUST greater than ending time of ar[pos] in log(n) time using binary search
+    int lo=pos+1,hi=n-1,nextPos=n;
+    while (lo<=hi){
+        int mid=lo+(hi-lo)/2;
+        if(ar[mid][0]>=ar[pos][1]){
+            nextPos=mid;
+            hi=mid-1;
+        }else lo=mid+1;
     }
+    dp[pos]=max(ans,ar[pos][2]+help(ar,nextPos));
+    return dp[pos];
+}
+
+long long maxTaxiEarnings(int n, vector<vector<int>>& ar) {
+    memset(dp,-1,sizeof(dp));
+    for(auto &it:ar) it[2]+=it[1]-it[0];
+    sort(ar.begin(),ar.end());
+    return help(ar,0);
+}
 };
