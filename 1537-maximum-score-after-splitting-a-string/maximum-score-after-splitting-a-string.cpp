36b0d038cc1fcee2bc27e10388fe939fc1ff8c5c
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2024-12-08 00:25:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (18.92%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -2,39 +2,21 @@ class Solution {
 public:
     int maxScore(string s) {
         int n = s.length();
+        
+        int leftZeroes = 0, rightOnes = 0, res = 0;
 
-        vector<int>leftZeroes(n, 0), rightOnes(n, 0);
-
-        int count=0;
-
-        for(int i=0; i<n; i++)
-        {
-            if(s[i]=='0')
-            {
-                count++;
-            }
-            leftZeroes[i] = count;
+        for (char c : s) {
+            if (c == '1') rightOnes++;
         }
 
-        count=0;
-
-        for(int i=n-1; i>=0; i--)
-        {
-            if(s[i]=='1')
-            {
-                count++;
+        for (int i = 0; i < n - 1; i++) {
+            if (s[i] == '0') {
+                leftZeroes++;
+            } else {
+                rightOnes--;
             }
-            rightOnes[i] = count;
-        }
 
-        int start = leftZeroes[0] == 0 ? 1: 0,
-        end = rightOnes[n-1] == 0 ? n-2: n-1;
-
-        int res = 0;
-
-        for(; start<=end; start++)
-        {
-            res = max(res, leftZeroes[start]+rightOnes[start]);
+            res = max(res, leftZeroes + rightOnes);
         }
 
         return res;
