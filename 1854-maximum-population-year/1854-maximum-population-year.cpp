d0be02e3da9814b67d5d3d23bd34c95472f5e5bf
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2024-12-09 18:23:44 +0530

    Time: 0 ms (100%), Space: 11.6 MB (5.86%) - LeetHub

---
+++ 1854-maximum-population-year/1854-maximum-population-year.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maximumPopulation(vector<vector<int>>& logs) {
+        vector<int>prefixSum(2051, 0);
+
+        int res = 0;
+
+        for(auto it: logs)
+        {
+            prefixSum[it[0]]++;
+            prefixSum[it[1]]--;
+        }
+
+        for(int i=1950; i<=2050; i++)
+        {
+            prefixSum[i]+=prefixSum[i-1];
+
+            if(prefixSum[i]>prefixSum[res])
+            {
+                res = i;
+            }
+        }
+
+        return res;
+    }
+};
