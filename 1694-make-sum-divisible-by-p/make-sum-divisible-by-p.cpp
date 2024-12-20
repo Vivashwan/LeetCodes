6e34f6544a4eb97b403cf6f623dcbff4eab233b9
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2024-12-21 02:23:45 +0530

    Time: 83 ms (66.20%) | Memory: 87.9 MB (30.24%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
     int minSubarray(vector<int>& nums, int p) {
-        
         int n = nums.size();
-        int r = 0 ;
-        for(const int& x : nums) r = (r + x) % p ; // handling larger numbers and at the same time we get the remainder too.
 
-        unordered_map<int,int> mp;
-        mp[0] = -1 ; // since we store remainder as key and idx as value
+        int total = 0;
 
-        int csum = 0 , len = n ; if(r == 0 ) return 0 ;
-        for(int i=0 ; i<n ; i++)
+        for(auto it: nums)
         {
-            csum  = (csum + nums[i]) % p; //due to large numbers ,orelse we can use csum+=num[i]
-
-            int rem =  (csum - r + p ) % p ; // handling negative remainders
-
-            if(mp.count(rem)) len = min(len, i - mp[rem]);
-
-            mp[csum] = i ; // store the current remainder in map along with current index
+            total=(total+it)%p;
         }
-        return len == n ? -1 : len ;
+
+        int rem = total%p;
+
+        if(rem==0)
+        {
+            return 0;
+        }
+
+        unordered_map<int, int>mp;
+
+        mp[0] = -1;
+
+        int res = n, sum = 0;
+
+        for(int i=0; i<n; i++)
+        {
+            sum = (sum+nums[i])%p;
+
+            int check = (sum-rem+p)%p;
+
+            if(mp.find(check)!=mp.end())
+            {
+                res = min(res, i - mp[check]);
+            }
+
+            mp[sum] = i;
+        }
+
+        return res == n ? -1: res;
     }
 };
