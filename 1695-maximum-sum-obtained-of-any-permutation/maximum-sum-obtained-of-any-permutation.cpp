eaecbad3e87c0a9c29ddf76ed17a0ccd2ec9dc9b
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2024-12-21 00:57:56 +0530

    Time: 103 ms (30.81%) | Memory: 139.1 MB (32.23%) - LeetSync

--- 1695-maximum-sum-obtained-of-any-permutation/maximum-sum-obtained-of-any-permutation.cpp
+++ 1695-maximum-sum-obtained-of-any-permutation/maximum-sum-obtained-of-any-permutation.cpp
@@ -1,35 +1,36 @@
 class Solution {
 private:
-    int mod = 1e9+7;
+    int MOD = 1e9+7;
 public:
     int maxSumRangeQuery(vector<int>& nums, vector<vector<int>>& requests) {
         int n = nums.size();
-        vector<int> ind(n+1, 0);
 
-        // Apply the difference array method
-        for (auto& req : requests) {
-            ind[req[0]]++;
-            if (req[1] + 1 < n) ind[req[1] + 1]--;
+        vector<int>prefix(n, 0);
+
+        for(auto it: requests)
+        {
+            prefix[it[0]]++;
+
+            if(it[1]+1<n)
+            {
+                prefix[it[1]+1]--;
+            }
         }
 
-        // Convert difference array to prefix sum array
-        for (int i = 1; i < n; i++) {
-            ind[i] += ind[i-1];
+        for(int i=1; i<n; i++)
+        {
+            prefix[i]+=prefix[i-1];
         }
 
-        // Remove the extra element used for boundary conditions
-        ind.pop_back();
-
-        // Sort the nums and the frequency array
         sort(nums.begin(), nums.end());
-        sort(ind.begin(), ind.end());
+        sort(prefix.begin(), prefix.end());
 
-        // Calculate the maximum sum using the sorted arrays
         long long sum = 0;
         for (int i = 0; i < n; i++) {
-            sum = (sum + (long long) ind[i] * nums[i]) % mod;
+            sum = (sum + (long long) prefix[i] * nums[i]) % MOD;
         }
 
-        return sum;
+
+        return sum%MOD;
     }
 };
