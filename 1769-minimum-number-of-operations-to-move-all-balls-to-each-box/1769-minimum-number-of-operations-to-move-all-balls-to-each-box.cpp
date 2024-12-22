c5d452ee8e77dea15b76a3d6da1e54b5fb0c03d6
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2024-12-22 23:11:47 +0530

    Time: 0 ms (100%), Space: 12.5 MB (18.38%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        int n = boxes.size();
-
-        vector<int>pref(n, 0);
-
-        int ones = boxes[0] == '1';
-
-        for(int i=1; i<n; i++)
-        {
-            pref[i] = pref[i-1] + ones;
-
-            if(boxes[i]=='1')
-            {
-                ones++;
-            }
+        vector<int> left(boxes.size(), 0), right(boxes.size(), 0), ans(boxes.size(), 0);
+        int balls = 0;
+        
+        balls = boxes[0] - '0';
+        for(int i=1;i<boxes.size();i++){
+            left[i] = left[i-1] + balls;
+            balls += boxes[i] - '0';
         }
-
-        ones = 0;
-        int prev = 0;
-
-        for(int i=n-1; i>=0; i--)
-        {
-            pref[i] = pref[i]+prev;
-
-            if(boxes[i]=='1')
-            {
-                ones++;
-            }
-            prev+=ones;
+        
+        balls = boxes[boxes.size()-1] - '0';
+        for(int i=boxes.size()-2;i>=0;i--){
+            right[i] = right[i+1] + balls;
+            balls += boxes[i] - '0';
         }
-
-        return pref;
+        
+        for(int i=0;i<boxes.size();i++)
+            ans[i] = left[i] + right[i];
+        
+        return ans;
     }
 };
