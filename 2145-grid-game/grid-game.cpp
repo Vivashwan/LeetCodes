5c19a56b1fa83393119c0a9785d12164309a70e4
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2024-12-24 19:25:17 +0530

    Time: 7 ms (25.42%) | Memory: 75.5 MB (30.51%) - LeetSync

---
+++ 2145-grid-game/grid-game.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+        int n = grid[0].size();
+
+        vector<long long>firstRow(n), secondRow(n);
+
+        for(int i=0; i<n; i++)
+        {
+            firstRow[i]=grid[0][i];
+            secondRow[i]=grid[1][i];
+        }
+
+        for(int i=1; i<n; i++)
+        {
+            firstRow[i]+=firstRow[i-1], secondRow[i]+=secondRow[i-1];
+        }
+
+        long long res = LLONG_MAX;
+
+        for(int i=0; i<n; i++)
+        {
+            long long pathFollowed = i>0 ? secondRow[i-1]:0,
+            pathToFollow = firstRow[n-1]-firstRow[i];
+
+            long long maxi = max(pathToFollow, pathFollowed);
+
+            res = min(maxi, res);
+        }
+
+        return res;
+    }
+};
