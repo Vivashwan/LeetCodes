44c2889a87c71216b393807b7d1971d0479def2b
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-02-03 16:02:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (57.71%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,54 +1,45 @@
 class Solution {
 private:
-    int func(string &s, int n, vector<int>&dp)
+    int func(string &s, int n)
     {
         if(s[0]=='0') 
         {
             return 0;
         }
 
-        dp[n]=1;
-
-        int count = 0;
-
-        string str = "";
+        int prev2=1, prev1 = 1;
 
         for(int i=n-1; i>=0; i--)
         {
-            if(s[i]=='0')
+            int curr = 0;
+
+            if(s[i]!='0')
             {
-                dp[i]=0;
-                continue;
-            }
-
-            dp[i]=dp[i+1];
-
-            if(i+1<n) 
-            {
-                int twoDigit=stoi(s.substr(i, 2));
-
-                if(twoDigit>=10 && twoDigit<=26) 
+                curr = prev1;
+            
+                if(i+1<n) 
                 {
-                    dp[i]+=dp[i+2];
+                    int twoDigit=stoi(s.substr(i, 2));
+
+                    if(twoDigit>=10 && twoDigit<=26) 
+                    {
+                        curr+=prev2;
+                    }
                 }
             }
+
+            prev2 = prev1;
+            prev1 = curr;
+            
         }
 
-        return dp[0];
+        return prev1;
     }
 public:
     int numDecodings(string s) {
         int n = s.length();
 
-        vector<int>dp(n+1, 0);
-
-        int count = func(s, n, dp);
-
-        for(auto it: dp)
-        {
-            cout<<it<<" ";
-        }
-        cout<<endl;
+        int count = func(s, n);
 
         return count;
     }
