f44c5d40a155b1b3105e7f6196fb2a8f5ae2dfbd
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-02-04 20:01:25 +0530

    Time: 256 ms (6.31%) | Memory: 100.3 MB (5.15%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/wiggle-subsequence.cpp
@@ -1,41 +1,35 @@
 class Solution {
-private:
-    int func(vector<int>& nums, int ind, int prev, bool positive, vector<vector<vector<int>>>& dp) 
-    {
-        if(ind == nums.size()) 
-        {
-            return 0; 
-        }
-
-        if(dp[ind][prev][positive]!=-1)
-        {
-            return dp[ind][prev][positive];
-        }
-        
-        int pick = 0;
-
-        if((positive && nums[ind] > nums[prev]) || (!positive && nums[ind] < nums[prev])) 
-        {
-            pick = 1 + func(nums, ind + 1, ind, !positive, dp); 
-        }
-        
-        int notPick = func(nums, ind + 1, prev, positive, dp); 
-
-        return dp[ind][prev][positive] = max(pick, notPick);
-    }
-
 public:
     int wiggleMaxLength(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1)
-        { 
+
+        if(n==1) 
+        {
             return 1;
         }
 
-        vector<vector<vector<int>>>dp;
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(n+1, vector<int>(2, 0)));
 
-        dp.resize(n+1, vector<vector<int>>(n+1, vector<int>(2, -1)));
+        for(int ind=n-1; ind>=0; ind--) 
+        {
+            for(int prev=ind-1; prev>=-1; prev--) 
+            {  
+                for(int positive=0; positive<2; positive++) 
+                {
+                    int pick=0;
 
-        return 1 + max(func(nums, 1, 0, true, dp), func(nums, 1, 0, false, dp)); 
+                    if((positive && (prev==-1 || nums[ind]>nums[prev])) || 
+                        (!positive && (prev==-1 || nums[ind]<nums[prev]))) {
+                        pick = 1 + dp[ind+1][ind+1][!positive]; 
+                    }
+
+                    int notPick = (ind+1<n) ? dp[ind+1][prev+1][positive]:0;
+                    
+                    dp[ind][prev+1][positive] = max(pick, notPick);
+                }
+            }
+        }
+        
+        return max(dp[0][0][0], dp[0][0][1]);
     }
 };
