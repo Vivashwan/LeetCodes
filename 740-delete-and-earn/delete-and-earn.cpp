2063ec45d31b30bb8feb8de6f47c88ff5e94a1bb
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-02-05 18:52:00 +0530

    Time: 14 ms (15.81%) | Memory: 19.2 MB (20.52%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -1,28 +1,23 @@
 class Solution {
 private:
-    int func(vector<int>&nums, int n, unordered_map<int, vector<int>>&mp, int ind, vector<int>&dp)
+    int func(vector<int>&nums, int n, unordered_map<int, vector<int>>&mp, vector<int>&dp)
     {
-        if(ind>=n)
+        for(int ind=n-1; ind>=0; ind--)
         {
-            return 0;
+            int lastInd = mp[nums[ind]].back();
+
+            if(mp[nums[ind]+1].size()>0)
+            {
+                lastInd = mp[nums[ind]+1].back();
+            }
+
+            int pick = nums[ind]*mp[nums[ind]].size()+dp[lastInd+1];
+            int notPick = dp[ind+1];
+
+            dp[ind] = max(pick, notPick);
         }
 
-        if(dp[ind]!=-1)
-        {
-            return dp[ind];
-        }
-
-        int lastInd = mp[nums[ind]].back();
-
-        if(mp[nums[ind]+1].size()>0)
-        {
-            lastInd = mp[nums[ind]+1].back();
-        }
-
-        int pick = nums[ind]*mp[nums[ind]].size()+func(nums, n, mp, lastInd+1, dp);
-        int notPick = func(nums, n, mp, ind+1, dp);
-
-        return dp[ind] = max(pick, notPick);
+        return dp[0];
     }
 public:
     int deleteAndEarn(vector<int>& nums) {
@@ -37,8 +32,8 @@ public:
             mp[nums[i]].push_back(i);
         }
 
-        vector<int>dp(n, -1);
+        vector<int>dp(n+1, 0);
 
-        return func(nums, n, mp, 0, dp);
+        return func(nums, n, mp, dp);
     }
 };
