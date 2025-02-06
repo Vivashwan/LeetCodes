dcd9c15604a371be3c556cfe333effd4c0fd2a5b
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-02-06 18:35:29 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (55.74%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,46 +1,41 @@
 class Solution {
 private:
-    int func(vector<int>&days, vector<int>&costs, int n, int ind, vector<int>&dp)
+    int func(vector<int>&days, vector<int>&costs, int n, vector<int>&dp)
     {
-        if(ind>=n)
+        for(int ind=n-1; ind>=0; ind--)
         {
-            return 0;
+            int expenseFor1DayPass = costs[0]+dp[ind+1];
+
+            int j=ind;
+
+            while(j<n && days[j]<days[ind]+7)
+            {
+                j++;
+            }
+
+            int expenseFor7DayPass = costs[1]+dp[j];
+
+            j=ind;
+
+            while(j<n && days[j]<days[ind]+30)
+            {
+                j++;
+            }
+
+            int expenseFor30DayPass = costs[2]+dp[j];
+
+            dp[ind] = min({expenseFor1DayPass, expenseFor7DayPass, expenseFor30DayPass});
+
         }
 
-        if(dp[ind]!=-1)
-        {
-            return dp[ind];
-        }
-
-        int expenseFor1DayPass = costs[0]+func(days, costs, n, ind+1, dp);
-
-        int j=ind;
-
-        while(j<n && days[j]<days[ind]+7)
-        {
-            j++;
-        }
-
-        int expenseFor7DayPass = costs[1]+func(days, costs, n, j, dp);
-
-        j=ind;
-
-        while(j<n && days[j]<days[ind]+30)
-        {
-            j++;
-        }
-
-        int expenseFor30DayPass = costs[2]+func(days, costs, n, j, dp);
-
-        return dp[ind] = min({expenseFor1DayPass, expenseFor7DayPass, expenseFor30DayPass});
-
+        return dp[0];
     }
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
 
-        vector<int>dp(n, -1);
+        vector<int>dp(n+1, 0);
 
-        return func(days, costs, n, 0, dp);
+        return func(days, costs, n, dp);
     }
 };
