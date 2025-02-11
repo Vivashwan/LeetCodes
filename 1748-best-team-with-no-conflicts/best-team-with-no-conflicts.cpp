2f491c97cba80d5dac71b2e6a8f997fca383c372
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-02-11 19:10:05 +0530

    Time: 20 ms (91.35%) | Memory: 23.6 MB (91.54%) - LeetSync

--- 1748-best-team-with-no-conflicts/best-team-with-no-conflicts.cpp
+++ 1748-best-team-with-no-conflicts/best-team-with-no-conflicts.cpp
@@ -2,30 +2,36 @@ class Solution {
 public:
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
         int n = scores.size();
-        
-        vector<pair<int, int>> vc(n);
-        for (int i = 0; i < n; ++i)
-            vc[i] = {ages[i], scores[i]};
-        
-        sort(vc.begin(), vc.end());
 
-        return highestScore(n, vc);
-    }
+        vector<int>dp(n, 0);
 
-    int highestScore(int n, vector<pair<int, int>> &vc) {
-        int maxScore = 0;
-        vector<int> dp(n);
+        vector<pair<int, int>>arr(n, {0, 0});
 
-        for (int i = 0; i < n; ++i) {
-            dp[i] = vc[i].second;
-            
-            for (int j = i - 1; j >= 0; --j) 
-                if (vc[i].second >= vc[j].second)
-                    dp[i] = max(dp[i], dp[j] + vc[i].second);
-            
-            maxScore = max(maxScore, dp[i]);
+        for(int i=0; i<n; i++)
+        {
+            arr[i].first=ages[i];
+            arr[i].second=scores[i];
         }
 
-        return maxScore;
+        sort(arr.begin(), arr.end());
+
+        int score=0;
+
+        for(int i=0; i<n; i++)
+        {
+            dp[i]=arr[i].second;
+
+            for(int j=i-1; j>=0; j--)
+            {
+                if(arr[i].second>=arr[j].second)
+                {
+                    dp[i]=max(dp[i], dp[j]+arr[i].second);
+                }
+            }
+
+            score=max(score, dp[i]);
+        }
+
+        return score;
     }
 };
