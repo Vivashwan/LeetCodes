c30d738ca1249cf02b1a10dc64cdfd7b1f19e4a0
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-02-13 17:29:49 +0530

    Time: 8 ms (29.56%) | Memory: 17.6 MB (45.32%) - LeetSync

--- 815-champagne-tower/champagne-tower.cpp
+++ 815-champagne-tower/champagne-tower.cpp
@@ -2,29 +2,34 @@ class Solution {
 private:
     double func(int poured, int row, int col, vector<vector<double>>&dp)
     {
-        if(row<0 || col<0 || col>row)
+        dp[0][0]=(double)poured;
+
+        for(int i=1; i<=row; i++)
         {
-            return 0.0;
+            for(int j=0; j<=i; j++)
+            {
+                double left = 0.0, right = 0.0;
+                
+                if(j>0) 
+                {
+                    left=max(0.0, (dp[i-1][j-1]-1)/2.0);
+                }
+
+                if(j<i) 
+                {
+                    right=max(0.0, (dp[i-1][j]-1)/2.0);
+                }
+
+                dp[i][j] = left+right;
+
+            }
         }
 
-        if(row==0 && col==0)
-        {
-            return poured;
-        }
-
-        if(dp[row][col]!=-1)
-        {
-            return dp[row][col];
-        }
-
-        double left = max(0.0, (func(poured, row - 1, col - 1, dp) - 1) / 2.0);
-        double right = max(0.0, (func(poured, row - 1, col, dp) - 1) / 2.0);
-
-        return dp[row][col] = left+right;
+        return dp[row][col];
     }
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
-        vector<vector<double>>dp(query_row+1, vector<double>(query_glass+1, -1));
+        vector<vector<double>>dp(query_row+1, vector<double>(query_row+1, 0.0));
         return min(1.0, func(poured, query_row, query_glass, dp));
     }
 };
