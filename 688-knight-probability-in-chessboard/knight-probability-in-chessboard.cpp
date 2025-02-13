de4c0eb43f60a603e854b98c750ff6966dde7145
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-02-13 15:03:55 +0530

    Time: 7 ms (71.75%) | Memory: 11.3 MB (70.35%) - LeetSync

--- 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
@@ -6,33 +6,40 @@ public:
 
     double dfs(int n, int k, int row, int col, vector<vector<vector<double>>>&dp) 
     {
-        if(row<0 || col<0 || row>=n || col>=n) 
+        for(int i=0; i<n; i++)
         {
-            return 0.0; 
+            for(int j=0; j<n; j++)
+            {
+                dp[i][j][0]=1.0;
+            }
         }
 
-        if(k==0)
+        for(int steps=1; steps<=k; steps++)
         {
-            return 1.0;
+            for(int i=0; i<n; i++)
+            {
+                for(int j=0; j<n; j++)
+                {
+                    double res=0.0;
+
+                    for(auto &it: dir) 
+                    {
+                        if(i+it[0]>=0 && j+it[1]>=0 && i+it[0]<n && j+it[1]<n) 
+                        {
+                            res+=(dp[i+it[0]][j+it[1]][steps-1])/8.0; 
+                        }
+                    }
+
+                    dp[i][j][steps] = res;
+                }
+            }
         }
 
-        if(dp[row][col][k]!=-1.0)
-        {
-            return dp[row][col][k];
-        }
-
-        double res=0.0;
-
-        for(auto &it: dir) 
-        {
-            res+=dfs(n, k-1, row+it[0], col+it[1], dp)/8.0;
-        }
-
-        return dp[row][col][k] = res;
+        return dp[row][col][k];
     }
 
     double knightProbability(int n, int k, int row, int col) {
-        vector<vector<vector<double>>>dp(n+1, vector<vector<double>>(n+1, vector<double>(k+1, -1.0)));
+        vector<vector<vector<double>>>dp(n+1, vector<vector<double>>(n+1, vector<double>(k+1, 0)));
         
         return dfs(n, k, row, col, dp);
     }
