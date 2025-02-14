261bfe0fb60ecf9746a12a23d4099ca7d398ef15
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-02-14 20:19:02 +0530

    Time: 267 ms (5.12%), Space: 70.6 MB (5.12%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,38 +1,41 @@
 class Solution {
-public:
-    int stoneGameII(vector<int>& piles) {
-        int n = piles.size();
-        
-        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(201, vector<int>(2, 0)));
-        
-        for(int ind=n-1; ind>=0; ind--) 
+private:
+    int func(vector<int>&piles, int n, vector<vector<vector<int>>>&dp)
+    {
+        for(int ind=n-1; ind>=0; ind--)
         {
-            for(int M=n; M>=1; M--) 
+            for(int M=1; M<=n; M++)
             {
-                for(int alice=0; alice<=1; alice++) 
+                for(int alice=0; alice<=1; alice++)
                 {
                     int res=(alice==1) ? INT_MIN:INT_MAX;
-                    int stones=0;
-                    
-                    for(int X=1; X<=min(2*M, n-ind); X++) 
-                    {
-                        stones+=piles[ind+X-1];  
 
-                        if(alice==1) 
+                    int stones=0;
+
+                    for(int X=1; X<=min(2*M, n-ind); X++)
+                    {
+                        stones+=piles[ind+(X-1)];
+
+                        if(alice==1)
                         {
-                            res=max(res, stones+dp[ind+X][max(M, X)][0]);
-                        } 
-                        else 
-                        {
-                            res=min(res, dp[ind+X][max(M, X)][1]);
+                            res=max(res, stones+dp[ind+X][max(M,X)][0]);
                         }
+                        else res=min(res, dp[ind+X][max(M,X)][1]);
                     }
 
-                    dp[ind][M][alice]=res;
+                    dp[ind][M][alice] = res;
+
                 }
             }
         }
-        
+
         return dp[0][1][1];
+    }
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n=piles.size();
+
+        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(201, vector<int>(2, 0)));
+        return func(piles, n, dp);
     }
 };
