ae8e546128a7e871130b000ae7462a55bc840360
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-02-15 16:00:33 +0530

    Time: 15 ms (55.09%), Space: 11.1 MB (72.13%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -3,37 +3,32 @@ private:
     int MOD=1e9+7;
     int func(int n, int k, int target, vector<vector<int>>&dp)
     {
-        if(target==0 && n==0)
-        {
-            return 1;
-        }
+        dp[0][0]=1;
 
-        if((target!=0 && n==0) || (target==0 && n!=0))
+        for(int i=1; i<=n; i++)
         {
-            return 0;
-        }
-
-        if(dp[n][target]!=-1)
-        {
-            return dp[n][target];
-        }
-
-        int count=0;
-
-        for(int val=1; val<=k; val++)
-        {
-            if(val<=target)
+            for(int j=1; j<=target; j++)
             {
-                count=(count+func(n-1, k, target-val, dp))%MOD;
+                int count=0;
+
+                for(int val=1; val<=k; val++)
+                {
+                    if(val<=j)
+                    {
+                        count=(count+dp[i-1][j-val])%MOD;
+                    }
+                }
+
+                dp[i][j] = count;
             }
         }
 
-        return dp[n][target] = count;
+        return dp[n][target];
     }
 
 public:
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>>dp(n+1, vector<int>(target+1, -1));
+        vector<vector<int>>dp(n+1, vector<int>(target+1, 0));
         return func(n, k, target, dp);
     }
 };
