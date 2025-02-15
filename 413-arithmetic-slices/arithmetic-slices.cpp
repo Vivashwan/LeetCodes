b0aa917f259016d2d4a72502e522a2a730f2d2a5
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-02-15 17:08:41 +0530

    Time: 43 ms (5.02%) | Memory: 152.9 MB (5.94%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.cpp
+++ 413-arithmetic-slices/arithmetic-slices.cpp
@@ -2,10 +2,12 @@ class Solution {
 private:
     int func(vector<int>&nums, int n, int curr, int prev, int diff, vector<vector<int>>&dp)
     {
+
         if(dp[curr][prev]!=-1)
         {
             return dp[curr][prev];
         }
+
         if(curr>=n || nums[curr]-nums[prev]!=diff)
         {
             return 0;
@@ -13,15 +15,9 @@ private:
 
         return dp[curr][prev] = 1+func(nums, n, curr+1, curr, diff, dp);
     }
-
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
-        int n=nums.size();
-
-        if(n<3)
-        {
-            return 0;
-        }
+        int n = nums.size();
 
         int count=0;
 
@@ -29,8 +25,7 @@ public:
 
         for(int i=0; i<n-2; i++)
         {
-            int diff=nums[i+1]-nums[i];
-            count+=func(nums, n, i+2, i+1, diff, dp);
+            count+=func(nums, n, i+2, i+1, nums[i+1]-nums[i], dp);
         }
 
         return count;
