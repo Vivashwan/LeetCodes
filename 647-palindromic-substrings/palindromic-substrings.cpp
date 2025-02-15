6c208f36564fc511bb1415be6e7558c82a7c0198
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-02-15 18:44:14 +0530

    Time: 9 ms (62.63%) | Memory: 21.2 MB (32.54%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,32 +1,23 @@
 class Solution {
+private:
+    void expand(string s, int n, int left, int right, int &count) 
+    {
+        while(left>=0 && right<n && s[left]==s[right]) 
+        {
+            count++;
+            left--, right++;
+        }
+    }
+
 public:
     int countSubstrings(string s) {
         int n=s.length();
-
         int count=0;
 
-        for(int i=0; i<n; i++)
+        for(int i=0; i<n; i++) 
         {
-            for(int j=i; j>=0; j--)
-            {
-                int left=j, right=i, flag=1;
-
-                while(left<=right)
-                {
-                    if(s[left]!=s[right])
-                    {
-                        flag=0;
-                        break;
-                    }
-
-                    left++, right--;
-                }
-
-                if(flag)
-                {
-                    count++;
-                }
-            }
+            expand(s, n, i, i, count);    
+            expand(s, n, i, i+1, count); 
         }
 
         return count;
