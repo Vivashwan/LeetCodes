fc1d0b137be0ce63fdb75fbe39bfcdaaef158db1
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-02-15 17:41:14 +0530

    Time: 5 ms (35.79%) | Memory: 11.2 MB (8.64%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    int func(vector<int>&nums, int n, int start, int end, int turn)
+    int func(vector<int>&nums, int n, int start, int end, int turn, vector<vector<vector<int>>>&dp)
     {
         if(start==n || end==-1)
         {
@@ -12,21 +12,28 @@ private:
             return 0;
         }
 
+        if(dp[start][end][turn]!=-1)
+        {
+            return dp[start][end][turn];
+        }
+
         if(turn==0)
         {
-            return max(nums[start]+func(nums, n, start+1, end, 1), nums[end]+func(nums, n, start, end-1, 1));
+            return dp[start][end][turn] = max(nums[start]+func(nums, n, start+1, end, 1, dp), nums[end]+func(nums, n, start, end-1, 1, dp));
         }
         else
         {
-            return min(-nums[start]+func(nums, n, start+1, end, 0), -nums[end]+func(nums, n, start, end-1, 0));
+            return dp[start][end][turn] = min(-nums[start]+func(nums, n, start+1, end, 0, dp), -nums[end]+func(nums, n, start, end-1, 0, dp));
         }
     }
-    
+
 public:
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
 
-        int val = func(nums, n, 0, n-1, 0);
+        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(n+1, vector<int>(2, -1)));
+
+        int val = func(nums, n, 0, n-1, 0, dp);
 
         return val>=0;
     }
