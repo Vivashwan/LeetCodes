c18eb02118aea8248013dfef4fd01941ad87716c
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-02-16 15:15:18 +0530

    Time: 299 ms (5.91%) | Memory: 119.4 MB (5.01%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,33 +1,48 @@
 class Solution {
 private:
-    int func(vector<int>&piles, int n, int start, int end, bool alice, vector<vector<vector<int>>>&dp)
+    int func(vector<int>& piles, int n, vector<vector<vector<int>>>&dp) 
     {
-        if(start>end)
+        for(int i=0; i<n; i++) 
         {
-            return alice?INT_MIN:INT_MAX;
+            dp[i][i][1]=piles[i];
+            dp[i][i][0]=0; 
+        }
+        
+        for(int start=n-1; start>=0; start--)
+        {
+            for(int end=start; end<n; end++) 
+            {
+                for(int alice=0; alice<2; alice++) 
+                {
+                    int aliceStones=0, bobStones=0;
+
+                    int nextStart=(start+1<n) ? dp[start+1][end][0] : 0;
+                    int prevEnd=(end-1>=0) ? dp[start][end-1][0] : 0;
+
+                    if(alice) 
+                    {
+                        aliceStones=max(piles[start]+nextStart, piles[end]+prevEnd);
+                    } 
+                    else 
+                    {
+                        nextStart=(start+1<n) ? dp[start+1][end][1] : 0;
+                        prevEnd=(end-1>=0) ? dp[start][end-1][1] : 0;
+                        bobStones=min(nextStart, prevEnd);
+                    }
+
+                    dp[start][end][alice] = alice ? aliceStones:bobStones;
+                }
+            }
         }
 
-        if(dp[start][end][alice]!=-1)
-        {
-            return dp[start][end][alice];
-        }
-
-        int aliceStones=0, bobStones=0;
-
-        if(alice)
-        {
-            aliceStones=max(piles[start]+func(piles, n, start+1, end, 0, dp), piles[end]+func(piles, n, start, end-1, 0, dp));
-        }
-        else bobStones=min(piles[start]+func(piles, n, start+1, end, 1, dp), piles[end]+func(piles, n, start, end-1, 1, dp));
-
-        return dp[start][end][alice] = aliceStones>bobStones;
+        return dp[0][n-1][1] > dp[0][n-1][0];  
     }
+
 public:
     bool stoneGame(vector<int>& piles) {
         int n=piles.size();
+        vector<vector<vector<int>>>dp(n, vector<vector<int>>(n, vector<int>(2, 0)));
 
-        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(n+1, vector<int>(2, -1)));
-
-        return func(piles, n, 0, n-1, 1, dp);
+        return func(piles, n, dp);
     }
 };
