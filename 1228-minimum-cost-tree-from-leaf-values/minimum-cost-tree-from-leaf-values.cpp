25f6f9719bf07c096b790d516428675bf7355b8e
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-02-18 15:28:44 +0530

    Time: 11 ms (68.17%) | Memory: 11.8 MB (83.52%) - LeetSync

--- 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
+++ 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
@@ -1,47 +1,44 @@
 class Solution {
 private:
-    int maxNonLeaf(vector<int>&arr, int start, int end)
+    int maxNonLeaf(vector<int>&arr, int start, int end) 
     {
-        int maxim = 0;
-
-        for(int i=start; i<=end; i++)
+        int maxim=0;
+        for (int i=start; i<=end; i++) 
         {
-            maxim = max(maxim, arr[i]);
+            maxim=max(maxim, arr[i]);
         }
 
         return maxim;
     }
 
-    int func(vector<int>&arr, int n, int start, int end, vector<vector<int>>&dp)
+    int func(vector<int>&arr, int n, vector<vector<int>>&dp) 
     {
-        if(start==end)
-        {
-            return 0;
-        }   
+        for(int length=2; length<=n; length++) 
+        { 
+            for(int start=0; start<=n-length; start++) 
+            {
+                int end=start+length-1;
 
-        if(dp[start][end]!=-1)
-        {
-            return dp[start][end];
+                dp[start][end]=INT_MAX;
+
+                for(int i=start; i<end; i++) 
+                { 
+                    int leftMax=maxNonLeaf(arr, start, i);
+                    int rightMax=maxNonLeaf(arr, i+1, end);
+
+                    dp[start][end] = min(dp[start][end], dp[start][i]+dp[i+1][end]+leftMax*rightMax);
+                }
+            }
         }
 
-        int res=INT_MAX;
-
-        for(int i=start; i<end; i++)
-        {
-            int leftMax=maxNonLeaf(arr, start, i);
-            int rightMax=maxNonLeaf(arr, i+1, end);
-
-            res=min(res, func(arr, n, start, i, dp)+func(arr, n, i+1, end, dp)+leftMax*rightMax);
-        }
-
-        return dp[start][end] = res;
+        return dp[0][n-1];
     }
+
 public:
     int mctFromLeafValues(vector<int>& arr) {
-        int n = arr.size();
+        int n=arr.size();
+        vector<vector<int>>dp(n, vector<int>(n, 0));
 
-        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-
-        return func(arr, n, 0, n-1, dp);
+        return func(arr, n, dp);
     }
 };
