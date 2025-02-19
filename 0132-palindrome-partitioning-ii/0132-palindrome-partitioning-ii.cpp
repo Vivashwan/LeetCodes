7a0a867ba0098b3a14eab56a6a33f9cbf5d4cdbb
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-02-19 14:42:50 +0530

    Time: 1521 ms (34.69%), Space: 9 MB (98.35%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -14,37 +14,34 @@ private:
         return true;
     }
 
-    int func(string &s, int left, int n, vector<int>&dp) 
+    int func(string &s, int n, vector<int>&dp) 
     {
-        if(left==n)
-        { 
-            return -1;
-        }
+        dp[n]=-1;
 
-        if(dp[left]!=-1)
+        for(int left=n-1; left>=0; left--)
         {
-            return dp[left];
-        }
+            int minCuts=INT_MAX;
 
-        int minCuts=INT_MAX;
-
-        for(int right=left; right<n; right++) 
-        {
-            if(isPalindrome(s, left, right)) 
+            for(int right=left; right<n; right++) 
             {
-                int cuts=1+func(s, right+1, n, dp);
-                minCuts=min(minCuts, cuts);
+                if(isPalindrome(s, left, right)) 
+                {
+                    int cuts=1+dp[right+1];
+                    minCuts=min(minCuts, cuts);
+                }
             }
+
+            dp[left] = minCuts;
         }
 
-        return dp[left] = minCuts;
+        return dp[0];
     }
 
 public:
     int minCut(string s) {
         int n=s.length();
 
-        vector<int>dp(n+1, -1);
-        return func(s, 0, n, dp);
+        vector<int>dp(n+1, 0);
+        return func(s, n, dp);
     }
 };
