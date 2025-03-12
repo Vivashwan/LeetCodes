98f05105efcb52a958928fe77de47d05eb3a306c
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-03-13 02:08:34 +0530

    Time: 83 ms (59.72%) | Memory: 89.3 MB (48.86%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
@@ -1,40 +1,17 @@
 class Solution {
-private:
-    int func(vector<int>&nums1, vector<int>&nums2, int ind1, int ind2, vector<vector<int>>& dp) 
-    {
-        if(ind1>=nums1.size() || ind2>=nums2.size()) 
-        {    
-            return 0;
-        } 
-
-        if(dp[ind1][ind2]!=-1) 
-        {    
-            return dp[ind1][ind2];
-        }
-
-        int res=0;
-
-        if(nums1[ind1]==nums2[ind2]) 
-        {
-            res = 1+func(nums1, nums2, ind1+1, ind2+1, dp);
-        }
-
-        return dp[ind1][ind2]=res;
-    }
-
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size(), m=nums2.size();
+        int n = nums1.size(), m = nums2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0)); // DP Table
+        
+        int maxLen = 0;
 
-        vector<vector<int>>dp(n, vector<int>(m, -1)); 
-
-        int maxLen=0;
-
-        for(int i=0; i<n; i++) 
-        {
-            for(int j=0; j<m; j++) 
-            {
-                maxLen = max(maxLen, func(nums1, nums2, i, j, dp));
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (nums1[i-1] == nums2[j-1]) { 
+                    dp[i][j] = 1 + dp[i-1][j-1]; // Extend previous subarray
+                    maxLen = max(maxLen, dp[i][j]); // Track max length
+                }
             }
         }
 
