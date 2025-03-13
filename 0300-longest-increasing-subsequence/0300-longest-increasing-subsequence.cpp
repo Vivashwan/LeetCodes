ba953fe0eea4d797ac097ab14e548b43b77ddbef
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-03-14 00:18:05 +0530

    Time: 271 ms (32.1%), Space: 296.2 MB (27.7%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,34 +1,25 @@
 class Solution {
-private:
-    int func(vector<int>&nums, int n, int prev, int curr, vector<vector<int>>&dp)
-    {
-        if(curr>=n)
-        {
-            return 0;
-        }
-
-        if(dp[prev+1][curr]!=-1)
-        {
-            return dp[prev+1][curr];
-        }
-
-        int res=0;
-
-        if(prev==-1 || nums[curr]>nums[prev])
-        {
-            res=max(res, 1+func(nums, n, curr, curr+1, dp));
-        }
-
-        res=max(res, func(nums, n, prev, curr+1, dp));
-
-        return dp[prev+1][curr] = res;
-    }
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
 
-        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+        // Fill the table bottom-up
+        for (int i = n - 1; i >= 0; i--) {
+            for (int prev = i - 1; prev >= -1; prev--) {
+                // Skip current element
+                int skip = dp[i + 1][prev + 1];
 
-        return func(nums, n, -1, 0, dp);
+                // Include current element if it's valid
+                int take = 0;
+                if (prev == -1 || nums[i] > nums[prev]) {
+                    take = 1 + dp[i + 1][i + 1];
+                }
+
+                dp[i][prev + 1] = max(skip, take);
+            }
+        }
+
+        return dp[0][0]; // LIS length starting from index 0
     }
 };
