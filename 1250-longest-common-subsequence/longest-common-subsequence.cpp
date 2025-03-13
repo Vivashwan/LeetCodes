444fa43a835ff93df3892be83a222e08921b96ae
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-03-13 21:09:10 +0530

    Time: 47 ms (12.09%) | Memory: 27.5 MB (28.34%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,34 +1,32 @@
 class Solution {
 private:
-    int func(string &text1, string &text2, int n, int m, int ind1, int ind2, vector<vector<int>>&dp)
+    int func(string &text1, string &text2, int n, int m, vector<vector<int>>&dp)
     {
-        if(ind1>=n || ind2>=m)
+        for(int ind1=1; ind1<=n; ind1++)
         {
-            return 0;
+            for(int ind2=1; ind2<=m; ind2++)
+            {
+                int res=0;
+
+                if(text1[ind1-1]==text2[ind2-1])
+                {
+                    res=1+dp[ind1-1][ind2-1];
+                }
+                else res=max({res, dp[ind1][ind2-1], dp[ind1-1][ind2]});
+
+                dp[ind1][ind2] = res;
+            }
         }
 
-        if(dp[ind1][ind2]!=-1)
-        {
-            return dp[ind1][ind2];
-        }
-
-        int res=0;
-
-        if(text1[ind1]==text2[ind2])
-        {
-            res=1+func(text1, text2, n, m, ind1+1, ind2+1, dp);
-        }
-        else res=max({res, func(text1, text2, n, m, ind1+1, ind2, dp), func(text1, text2, n, m, ind1, ind2+1, dp)});
-
-        return dp[ind1][ind2] = res;
+        return dp[n][m];
     }
 
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.length(), m=text2.length();
 
-        vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
 
-        return func(text1, text2, n, m, 0, 0, dp);
+        return func(text1, text2, n, m, dp);
     }
 };
