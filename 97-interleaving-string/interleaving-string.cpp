3cef3548a10ad2cee3c554e85f6e96fa29c37d3c
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-03-13 23:37:08 +0530

    Time: 38 ms (9.39%) | Memory: 18.6 MB (17.71%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,42 +1,28 @@
 class Solution {
-private:
-    bool func(string& s1, string& s2, string& s3, int n, int m, int l, int ind1, int ind2, int ind3, vector<vector<vector<int>>>&dp)
-    {
-        if(ind3>=l)
-        {
-            return true;
-        }
-
-        if(dp[ind1][ind2][ind3]!=-1)
-        {
-            return dp[ind1][ind2][ind3];
-        }
-
-        bool res=false;
-
-        if(ind1<n && s3[ind3]==s1[ind1])
-        {
-            res=res||func(s1, s2, s3, n, m, l, ind1+1, ind2, ind3+1, dp);
-        }
-
-        if(ind2<m && s3[ind3]==s2[ind2])
-        {
-            res=res||func(s1, s2, s3, n, m, l, ind1, ind2+1, ind3+1, dp);
-        }
-
-        return dp[ind1][ind2][ind3] = res;
-    }
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        int n = s1.length(), m = s2.length(), l = s3.length();
+        int n = s1.size(), m = s2.size(), l = s3.size();
+        if (n + m != l) return false;
 
-        if(n+m != l)
-        { 
-            return false; 
+        vector<vector<vector<bool>>> dp(n + 1, vector<vector<bool>>(m + 1, vector<bool>(l + 1, false)));
+        
+        dp[0][0][0] = true;
+        
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                for (int k = 0; k <= l; k++) {
+                    if (i + j != k) continue;
+
+                    if (i > 0 && s1[i - 1] == s3[k - 1]) {
+                        dp[i][j][k] = dp[i][j][k] || dp[i - 1][j][k - 1];
+                    }
+                    if (j > 0 && s2[j - 1] == s3[k - 1]) {
+                        dp[i][j][k] = dp[i][j][k] || dp[i][j - 1][k - 1];
+                    }
+                }
+            }
         }
 
-        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(m+1, vector<int>(l+1, -1)));
-
-        return func(s1, s2, s3, n, m, l, 0, 0, 0, dp);
+        return dp[n][m][l]; 
     }
 };
