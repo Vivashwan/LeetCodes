57598f3e16a4e10269e5973b636f903ba666d369
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-03-15 22:30:41 +0530

    Time: 12 ms (38.35%), Space: 31.1 MB (34.18%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,29 +1,9 @@
 class Solution {
-private:
-    int func(vector<vector<char>>& matrix, int n, int m, int row, int col, vector<vector<int>>&dp)
-    {
-        if(row<0 || col<0)
-        {
-            return 0;
-        }
-
-        if(dp[row][col]!=-1)
-        {
-            return dp[row][col];
-        }
-
-        if(matrix[row][col]=='0')
-        {
-            return 0;
-        }
-
-        return dp[row][col] = 1+min({func(matrix, n, m, row-1, col, dp), func(matrix, n, m, row, col-1, dp), func(matrix, n, m, row-1, col-1, dp)});
-    }
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        int n = matrix.size(), m = matrix[0].size();
+        int n=matrix.size(), m=matrix[0].size();
 
-        vector<vector<int>>dp(n, vector<int>(m, -1));
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
 
         int maxSide=0;
 
@@ -31,7 +11,19 @@ public:
         {
             for(int j=0; j<m; j++)
             {
-                maxSide=max(maxSide, func(matrix, n, m, i, j, dp));
+                if(matrix[i][j]=='1') 
+                {
+                    if(i==0 || j==0) 
+                    {
+                        dp[i][j]=1;
+                    } 
+                    else 
+                    {
+                        dp[i][j] = 1+min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
+                    }
+                }
+
+                maxSide = max(maxSide, dp[i][j]);
             }
         }
 
