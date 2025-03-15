319a5a0d337f89103d9d4f3122a962a58613e1a2
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-03-15 19:35:24 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (33.99%) - LeetSync

--- 1324-where-will-the-ball-fall/where-will-the-ball-fall.cpp
+++ 1324-where-will-the-ball-fall/where-will-the-ball-fall.cpp
@@ -1,44 +1,28 @@
 class Solution {
 public:
     vector<int> findBall(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        vector<int> res(m, -1);
+        int n=grid.size(), m=grid[0].size();
 
-        for(int j = 0; j < m; j++) 
+        vector<int>res(m, -1);  
+
+        for(int j=0; j<m; j++) 
         {
-            int col = j;
+            int col=j;  
 
-            for(int row = 0; row < n; row++) 
+            for(int row=0; row<n; row++) 
             {
-                if(grid[row][col] == 1) 
+                int nextCol = col+grid[row][col]; 
+
+                if(nextCol<0 || nextCol>=m || grid[row][col]!=grid[row][nextCol]) 
                 {
-                    if(col + 1 < m && grid[row][col + 1] == 1) 
-                    {
-                        col++;
-                    } else 
-                    {
-                        col = -1;
-                        break;
-                    }
-                } 
-                else if(grid[row][col] == -1) 
-                {
-                    if(col - 1 >= 0 && grid[row][col - 1] == -1) 
-                    {
-                        col--; 
-                    } 
-                    else 
-                    {
-                        col = -1; 
-                        break;
-                    }
+                    col=-1;
+                    break;
                 }
+                
+                col=nextCol;
             }
 
-            if(col != -1) 
-            {
-                res[j] = col; 
-            }
+            res[j] = col;
         }
 
         return res;
