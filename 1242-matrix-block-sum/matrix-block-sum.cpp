dbcbf4a6eb399bc626e4afffc8fc03d81ebac3f3
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-03-17 22:27:45 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (42.08%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.cpp
+++ 1242-matrix-block-sum/matrix-block-sum.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
-        int m = mat.size();
-        int n = mat[0].size();
-        
-        vector<vector<int>> prefix(m + 1, vector<int>(n + 1, 0));
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                prefix[i][j] = mat[i - 1][j - 1]
-                             + prefix[i - 1][j] 
-                             + prefix[i][j - 1] 
-                             - prefix[i - 1][j - 1];
+        int n = mat.size(), m=mat[0].size();
+
+        vector<vector<int>>prefix(n+1, vector<int>(m+1, 0));
+
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=m; j++)
+            {
+                prefix[i][j]=mat[i-1][j-1]+prefix[i-1][j]+prefix[i][j-1]-prefix[i-1][j-1];
             }
         }
-        
-        vector<vector<int>> answer(m, vector<int>(n, 0));
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int r1 = max(0, i - k), c1 = max(0, j - k);
-                int r2 = min(m - 1, i + k), c2 = min(n - 1, j + k);
-                
-                answer[i][j] = prefix[r2 + 1][c2 + 1]
-                             - prefix[r1][c2 + 1]
-                             - prefix[r2 + 1][c1]
-                             + prefix[r1][c1];
+
+        vector<vector<int>>res(n, vector<int>(m, 0));
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                int r1=max(0, i-k), r2=min(n-1, i+k), c1=max(0, j-k), c2=min(m-1, j+k);
+                res[i][j]=prefix[r2+1][c2+1]-prefix[r2+1][c1]-prefix[r1][c2+1]+prefix[r1][c1];
             }
         }
-        
-        return answer;
+
+        return res;
     }
 };
