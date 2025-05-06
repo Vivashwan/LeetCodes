7caa35cbc8fc7d49afb84480714e627bf51ea228
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-05-07 01:29:57 +0530

    Time: 95 ms (33.66%), Space: 84.9 MB (26.78%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,20 +1,41 @@
 class Solution {
 public:
     int minSetSize(vector<int>& arr) {
-          int n=*max_element(arr.begin(),arr.end());
-           vector<int>vec(n+1,0);
-           for(int i=0;i<arr.size();i++){
-               vec[arr[i]]++;
-           }
-           sort(vec.begin(),vec.end(),greater<int>());
-           int cnt=0;
-           int sum=0;
-           for(int i=0;i<vec.size();i++){
-                sum+=vec[i];
-                cnt++;
-                if(sum>=arr.size()/2)
+        int n = arr.size();
+        int size = n/2;
+
+        unordered_map<int, int>mp;
+
+        for(auto it: arr)
+        {
+            mp[it]++;
+        }
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>>pq;
+
+        for(auto it: mp)
+        {
+            pq.push({it.second, it.first});
+        }
+
+        int res=0, count=0;
+
+        while(!pq.empty())
+        {
+            auto u = pq.top();
+
+            pq.pop();
+
+            res++;
+
+            count+=u.first;
+
+            if(count>=size)
+            {
                 break;
-           }
-           return cnt;
+            }
+        }
+
+        return res;
     }
 };
