fa46089a07fee668c637484bafbf3a0de443bd3d
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-05-23 20:35:27 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (19.79%) - LeetSync

---
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCousins(TreeNode* root, int x, int y) {
+        if(!root)
+        { 
+            return false;
+        }
+
+        queue<pair<TreeNode*, TreeNode*>>q;
+
+        q.push({root, nullptr});
+
+        while(!q.empty()) 
+        {
+            int size=q.size();
+            TreeNode* x_parent=nullptr, *y_parent=nullptr;
+
+            for(int i=0; i<size; i++) 
+            {
+                auto [node, parent]=q.front();
+                q.pop();
+
+                if(node->val==x)
+                { 
+                    x_parent=parent;
+                }
+
+                if(node->val==y)
+                { 
+                    y_parent=parent;
+                }
+
+                if(node->left)
+                {    
+                    q.push({node->left, node});
+                }
+
+                if(node->right)
+                {    
+                    q.push({node->right, node});
+                }
+            }
+
+            if(x_parent && y_parent)
+            {    
+                return x_parent!=y_parent;
+            }
+
+            if(x_parent || y_parent)
+            {    
+                return false;
+            }
+        }
+
+        return false;
+    }
+};
