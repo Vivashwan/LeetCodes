774b991071802cef45926895e87a58708d006916
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-06-24 20:42:14 +0530

    Time: 80 ms (20.96%) | Memory: 42.5 MB (31.62%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.cpp
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -1,43 +1,45 @@
 class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        unordered_set<string>obstacleSet;
+        unordered_set<string>set;
 
-        for(auto obs: obstacles) 
+        for(auto it: obstacles)
         {
-            obstacleSet.insert(to_string(obs[0]) + "," + to_string(obs[1]));
+            set.insert(to_string(it[0])+","+to_string(it[1]));
         }
-        
-        vector<vector<int>>directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
 
-        int x=0, y=0, dir=0, maxDist=0;
+        vector<vector<int>>directions={{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
-        for(int cmd: commands) 
+        int x=0, y=0, maxRes=0, dir=0;
+
+        for(auto it: commands)
         {
-            if(cmd == -2) 
+            if(it==-2)
             {
-                dir = (dir + 3) % 4;
-            } 
-            else if(cmd == -1)
+                dir=(dir+3)%4;
+            }
+            else if(it==-1)
             {
-                dir = (dir + 1) % 4;
-            } 
-            else 
-            { 
-                for(int i=0; i<cmd; i++) 
+                dir=(dir+1)%4;
+            }
+            else
+            {
+                for(int i=0; i<it; i++)
                 {
-                    int newX = x+directions[dir][0];
-                    int newY = y+directions[dir][1];
-                    if(obstacleSet.find(to_string(newX) + "," + to_string(newY)) != obstacleSet.end()) {
+                    int newX=x+directions[dir][0];
+                    int newY=y+directions[dir][1];
+
+                    if(set.find(to_string(newX)+","+to_string(newY))!=set.end())
+                    {
                         break;
                     }
 
                     x=newX, y=newY;
-                    maxDist = max(maxDist, x*x + y*y);
+                    maxRes = max(maxRes, x*x + y*y);
                 }
             }
         }
-        
-        return maxDist;
+
+        return maxRes;
     }
 };
