8f868a14a18128bf9db248a543cddbc5bc251780
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-06-28 22:09:03 +0530

    Time: 91 ms (58.53%) | Memory: 27.7 MB (16.63%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -1,25 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
+private:
+    int MOD=1e9+7;
+
 public:
-    const int M = 1e9 + 7;
-    
-    int rangeSum(vector<int>& nums, int n, int l, int r) {
-        vector<int> sumsubarray;
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        vector<int>prefix(n+1, 0);
+
+        for(int i=0; i<n; i++)
+        {    
+            prefix[i+1]=prefix[i]+nums[i];
+        }
+
+        vector<int>subSums;
         
-        for (int i = 0; i < n; ++i) {
-            int k = 0;
-            for (int j = i; j < n; ++j) {
-                k += nums[j];
-                sumsubarray.push_back(k);
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i+1; j<=n; j++)
+            {
+                subSums.push_back(prefix[j]-prefix[i]);
             }
         }
-        
-        sort(sumsubarray.begin(), sumsubarray.end());
-        
-        int ans = 0;
-        for (int i = l - 1; i <= r - 1; ++i) {
-            ans = (ans + sumsubarray[i]) % M;
+
+        sort(subSums.begin(), subSums.end());
+
+        long long res=0;
+
+        for(int i=left-1; i<=right-1; i++)
+        {
+            res=(res+subSums[i])%MOD;
         }
-        
-        return ans;
+
+        return (int)res;
     }
 };
