dbb72eed99cda35e45b3dea2a08445b4407dc765
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-07-09 20:18:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (82.03%) - LeetSync

--- 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
+++ 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
@@ -1,44 +1,30 @@
 class Solution {
-private:
-    int maxNonLeaf(vector<int>&arr, int start, int end) 
-    {
-        int maxim=0;
-        for (int i=start; i<=end; i++) 
-        {
-            maxim=max(maxim, arr[i]);
-        }
-
-        return maxim;
-    }
-
-    int func(vector<int>&arr, int n, vector<vector<int>>&dp) 
-    {
-        for(int length=2; length<=n; length++) 
-        { 
-            for(int start=0; start<=n-length; start++) 
-            {
-                int end=start+length-1;
-
-                dp[start][end]=INT_MAX;
-
-                for(int i=start; i<end; i++) 
-                { 
-                    int leftMax=maxNonLeaf(arr, start, i);
-                    int rightMax=maxNonLeaf(arr, i+1, end);
-
-                    dp[start][end] = min(dp[start][end], dp[start][i]+dp[i+1][end]+leftMax*rightMax);
-                }
-            }
-        }
-
-        return dp[0][n-1];
-    }
-
 public:
     int mctFromLeafValues(vector<int>& arr) {
-        int n=arr.size();
-        vector<vector<int>>dp(n, vector<int>(n, 0));
+        int res=0;
+        stack<int>st;
 
-        return func(arr, n, dp);
+        st.push(INT_MAX);
+
+        for(auto it: arr)
+        {
+            while(!st.empty() && st.top()<=it)
+            {
+                int mid=st.top();
+                st.pop();
+                res+=mid*min(st.top(), it);
+            }
+            
+            st.push(it);
+        }
+
+        while(st.size()>2)
+        {
+            int mid=st.top();
+            st.pop();
+            res+=mid*st.top();
+        }
+
+        return res;
     }
 };
