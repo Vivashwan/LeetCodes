faa4bbc18607a35b46de2c9148b861b897460ed2
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-07-28 21:54:33 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (60.59%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>subs={{}};
+
+        for(auto it: nums)
+        { 
+            int n=subs.size();
+
+            for(int i=0; i<n; i++) 
+            {
+                subs.push_back(subs[i]); 
+                subs.back().push_back(it);
+            }
+        }
+
+        return subs;
+    }
+};
