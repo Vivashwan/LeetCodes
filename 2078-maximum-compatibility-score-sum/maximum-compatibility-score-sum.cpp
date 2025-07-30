23f16f1bc8cb240802c346c829a2d8cc705a1d78
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-07-30 16:33:34 +0530

    Time: 27 ms (70.83%) | Memory: 13.1 MB (68.40%) - LeetSync

--- 2078-maximum-compatibility-score-sum/maximum-compatibility-score-sum.cpp
+++ 2078-maximum-compatibility-score-sum/maximum-compatibility-score-sum.cpp
@@ -1,56 +1,56 @@
 class Solution {
 public:
-    
-    int calculate(vector<vector<int>>& students, vector<vector<int>>& mentors, vector<int>& arr)
+    int m, n;
+    vector<vector<int>>score;
+    int ans=0;
+
+    void backtrack(int studentIdx, vector<bool>& used, int currSum)
     {
-        int n = students.size();
-        
-        // find the compatability for curr_combination
-        
-        int sum = 0;
-        
-        for(int i = 0; i < n; i++)
-        {   
-            for(int j = 0; j < students[i].size(); j++)
+        if(studentIdx==m)
+        {
+            ans=max(ans, currSum);
+            return;
+        }
+
+        for(int mentor=0; mentor<m; mentor++)
+        {
+            if(!used[mentor])
             {
-                if(students[i][j] == mentors[arr[i]][j])
-                    sum++;
+                used[mentor]=true;
+                backtrack(studentIdx+1, used, currSum+score[studentIdx][mentor]);
+                used[mentor]=false;
             }
         }
-        
-        return sum;
     }
-    
+
     int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) {
-        
-        int n = students.size();
-        
-        // store the indexo of all mentors into array
-        
-        vector<int> arr(n);
-        
-        for(int i = 0; i < n; i++)
+        m=students.size();
+        n=students[0].size();
+
+        score.assign(m, vector<int>(m, 0));
+
+        for(int i=0; i<m; i++)
         {
-            arr[i] = i;
+            for(int j=0; j<m; j++)
+            {
+                int cnt=0;
+
+                for(int k=0; k<n; k++)
+                {
+                    if(students[i][k]==mentors[j][k])
+                    { 
+                        cnt++;
+                    }
+                }
+
+                score[i][j]=cnt;
+            }
         }
+
+        vector<bool>used(m, false);
+
+        backtrack(0, used, 0);
         
-        int maxi = INT_MIN;
-        
-        // for every combination of mentor find the comapatability and take maximum of it
-        
-        do
-        {
-            // calculate compatability
-            
-            int ans = calculate(students, mentors, arr);
-            
-            // update maxi
-            
-            maxi = max(maxi, ans);
-        }
-        
-        while(next_permutation(arr.begin(), arr.end()));
-        
-        return maxi;
+        return ans;
     }
 };
