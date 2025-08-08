a70ca4b7a088e5a1f5dd1eb7204d68f27199f647
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-08-08 16:26:45 +0530

    Time: 42 ms (28.01%) | Memory: 18.6 MB (52.98%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,11 +9,61 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* tempA = headA, *tempB = headB;
-        while(tempA != tempB) {
-            tempA = (tempA == NULL) ? headB : tempA->next;
-            tempB = (tempB == NULL) ? headA : tempB->next;
+        if(!headA)
+        {
+            return headB;
         }
-        return tempA;
+
+        if(!headB)
+        {
+            return headA;
+        }
+
+        ListNode* currA=headA;
+        int length1=0;
+
+        while(currA)
+        {
+            length1++;
+            currA=currA->next;
+        }
+
+        ListNode* currB=headB;
+        int length2=0;
+
+        while(currB)
+        {
+            length2++;
+            currB=currB->next;
+        }
+
+        currA=headA, currB=headB;
+
+        if(length1>length2)
+        {
+            int diff=length1-length2;
+
+            while(diff--)
+            {
+                currA=currA->next;
+            }
+        }
+        else if(length2>length1)
+        {
+            int diff=length2-length1;
+
+            while(diff--)
+            {
+                currB=currB->next;
+            }
+        }
+
+        while(currA && currB && currA!=currB)
+        {
+            currA=currA->next;
+            currB=currB->next;
+        }
+
+        return currA;
     }
 };
