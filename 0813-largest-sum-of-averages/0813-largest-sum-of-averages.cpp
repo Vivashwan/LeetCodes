3e6631e134853cc4512c369652871d7bf9a1a960
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-08-09 01:34:29 +0530

    Time: 3 ms (90.13%), Space: 11.2 MB (37.09%) - LeetHub

--- 0813-largest-sum-of-averages/0813-largest-sum-of-averages.cpp
+++ 0813-largest-sum-of-averages/0813-largest-sum-of-averages.cpp
@@ -1,37 +1,39 @@
 class Solution {
 private:
-    double func(vector<int>&nums, int n, int k, int left, vector<vector<double>>&dp)
+    double func(vector<int>&nums, int n, int left, int k, vector<vector<double>>&dp)
     {
         if(left>=n)
         {
             return 0.0;
         }
 
-        if(k==0)
-        {
-            return INT_MIN/2;
-        }        
-
-        if(dp[left][k]!=-1.0)
+        if(dp[left][k]!=-1)
         {
             return dp[left][k];
         }
 
-        double res=0.0, sum=0.0;
+        if(k==0)
+        {
+            return INT_MIN/2;
+        }
+
+        double sum=0.0, res=0.0; 
 
         for(int i=left; i<n; i++)
         {
             sum+=nums[i];
-            res=max(res, sum/(i-left+1)+func(nums, n, k-1, i+1, dp));
+
+            res=max(res, sum/(i-left+1)+func(nums, n, i+1, k-1, dp));
         }
 
-        return dp[left][k] = res;
+        return dp[left][k]=res;
     }
 public:
     double largestSumOfAverages(vector<int>& nums, int k) {
-        int n = nums.size();
+        int n=nums.size();
 
-        vector<vector<double>>dp(110, vector<double>(110, -1.0));
-        return func(nums, n, k, 0, dp);
+        vector<vector<double>>dp(n, vector<double>(k+1, -1));
+
+        return func(nums, n, 0, k, dp);
     }
 };
