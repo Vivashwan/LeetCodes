52e3f71477546bb2e23c4dbb1563e0176e8dfde9
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-08-09 23:57:06 +0530

    Time: 1 ms (82.55%) | Memory: 71.6 MB (81.89%) - LeetSync

--- 878-shifting-letters/shifting-letters.cpp
+++ 878-shifting-letters/shifting-letters.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     string shiftingLetters(string s, vector<int>& shifts) {
-        int n = s.length();
+        int n=s.length();
 
-        long total = 0;
+        long total=0;
 
         for(int i=n-1; i>=0; i--)
         {
-            s[i] = ((s[i]-'a')+(shifts[i]+total)%26)%26+'a';
-            total += shifts[i];
+            s[i]=((s[i]-'a')+(shifts[i]+total)%26)%26+'a';
+            total+=shifts[i];
         }
 
         return s;
