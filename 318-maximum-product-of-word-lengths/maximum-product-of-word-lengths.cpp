f3f06029cea2b004a3c4e26c08ed4d4b39288679
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-08-28 17:33:36 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (58.33%) - LeetSync

--- 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
     int maxProduct(vector<string>& words) {
-        int n = words.size();
-        vector<int> mask(n);
-        vector<int> len(n);
+        int n=words.size();
+        vector<int>mask(n), len(n);
 
-        for (int i = 0; i < n; i++) {
-            int m = 0;
-            for (char c : words[i]) {
-                m |= (1 << (c - 'a'));
+        for(int i=0; i<n; i++)
+        {
+            int m=0;
+
+            for(char c:words[i]) 
+            {
+                m|=(1<<(c-'a'));
             }
-            mask[i] = m;
-            len[i] = words[i].size();
+
+            mask[i]=m;
+            len[i]=words[i].size();
         }
 
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if ((mask[i] & mask[j]) == 0) {
-                    res = max(res, len[i] * len[j]);
+        int res=0;
+
+        for(int i=0; i<n; i++) 
+        {
+            for(int j=i+1; j<n; j++) 
+            {
+                if((mask[i] & mask[j])==0) 
+                {
+                    res=max(res, len[i]*len[j]);
                 }
             }
         }
+        
         return res;
     }
 };
