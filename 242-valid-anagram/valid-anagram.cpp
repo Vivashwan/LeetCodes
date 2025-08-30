d81843d9e5cb21fc1358185c963bc112d4f8eecf
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-08-30 19:41:19 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (91.05%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isAnagram(string str1, string str2) {
+
+        if(str1.length()!=str2.length())
+        {
+            return false;
+        }
+
+        vector<int>count(256, 0);
+
+        for (int i=0; i<str1.length(); i++)
+        {
+            count[str1[i]]++;
+            count[str2[i]]--;
+        }
+
+        for(int i=0; i<256; i++)
+        {
+            if(count[i]!=0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
