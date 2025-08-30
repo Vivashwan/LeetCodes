0a4776b160f70051cfbd2c34f44c7b570910c21f
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-08-30 15:16:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (95.21%) - LeetSync

---
+++ 2010-check-if-word-equals-summation-of-two-words/check-if-word-equals-summation-of-two-words.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isSumEqual(string firstWord, string secondWord, string targetWord) {
+        int n=firstWord.size(), m=secondWord.size(), l=targetWord.size();
+
+        int first=0, second=0, target=0;
+
+        for(int i=0; i<n; i++)
+        {    
+            first=first*10 + (firstWord[i]-'a');
+        }
+        
+        for(int i=0; i<m; i++)
+        {    
+            second=second*10 + (secondWord[i]-'a');
+        }
+        
+        for(int i=0;i<l;i++)
+        {    
+            target=target*10 + (targetWord[i]-'a');
+        }
+    
+        return first+second==target;
+    }
+};
