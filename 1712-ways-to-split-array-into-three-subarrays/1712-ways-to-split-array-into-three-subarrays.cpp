fd67d9e0fe07e3528441f17c3f431729f55cbb83
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-09-04 15:04:15 +0530

    Time: 10 ms (81.16%), Space: 89.4 MB (46.38%) - LeetHub

--- 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
+++ 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
@@ -1,78 +1,44 @@
 class Solution {
+private:
+    int MOD=1e9+7;
 public:
-        int mod = 1e9 + 7;
-    
-    int find_left_boundary(vector<int> &arr, int start, int end, int i, int n)
-    {
-        int left_boundary = n;
-        
-        while(start <= end)
+    int waysToSplit(vector<int>& nums) {
+        int n=nums.size();
+
+        vector<int>prefix(n+1, 0);
+
+        for(int i=0; i<n; i++)
         {
-            int mid = (start + end) / 2;
-            
-            if(arr[mid] >= 2 * arr[i])
+            prefix[i+1]=nums[i]+prefix[i];
+        }
+
+        int low=1, high=1;
+        long long res=0;
+
+        for(int i=1; i<=n-2; i++)
+        {
+            long long leftSubarray=prefix[i];
+
+            low=max(low, i+1);
+
+            while(low<n && prefix[low]<2*leftSubarray)
             {
-                left_boundary = mid;
-                
-                end = mid - 1;
+                low++;
             }
-            
-            else{
-                
-                start = mid + 1;
-            }
-        }
-        
-        return left_boundary;
-    }
-    
-    int find_right_boundary(vector<int> &arr, int start, int end, int i, int n)
-    {
-        int right_boundary = i;
-        
-        while(start <= end)
-        {
-            int mid = (start + end) / 2;
-            
-            if(arr[mid] <= (arr[n-1] + arr[i]) / 2)
+
+            high=max(high, low);
+
+            while(high<n && prefix[high]<=(prefix[n]+leftSubarray)/2)
             {
-                right_boundary = mid;
-                
-                start = mid + 1;
+                high++;
             }
-            
-            else{
-                
-                end = mid - 1;
-            }
-        }
-        
-        return right_boundary;
-    }
-    
-    int waysToSplit(vector<int>& arr) {
-        
-        int n = arr.size();
-        
-        int ans = 0;
-        
-        for(int i = 1; i < n; i++)
-        {
-            arr[i] += arr[i-1];
-        }
-        
-        for(int i = 0; i <= n - 3; i++)
-        {
-            int left_boundary = find_left_boundary(arr, i + 1, n - 2, i, n);
-            
-            int right_boundary = find_right_boundary(arr, i + 1, n - 2, i, n);
-            
-            if(right_boundary >= left_boundary)
+
+            if(low<=high-1)
             {
-                ans = (ans % mod + (right_boundary - left_boundary + 1) % mod ) % mod;
+                res=(res+(high-1-(low)+1))%MOD;
             }
         }
-        
-        return ans;
+
+        return (int)res;
     }
 };
