d1f5fd3b3887c31663120b6ce880dc3a74bcf298
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-09-09 22:46:06 +0530

    Time: 5 ms (27.54%), Space: 75.5 MB (26.45%) - LeetHub

--- 2017-grid-game/2017-grid-game.cpp
+++ 2017-grid-game/2017-grid-game.cpp
@@ -3,32 +3,33 @@ public:
     long long gridGame(vector<vector<int>>& grid) {
         int n=grid[0].size();
 
-        vector<long long>f(n),s(n);
+        vector<long long>firstRow(n), secondRow(n);
 
         for(int i=0; i<n; i++)
         {
-            f[i]=grid[0][i];
-            s[i]=grid[1][i];
+            firstRow[i]=grid[0][i];
+            secondRow[i]=grid[1][i];
         }
 
         for(int i=1; i<n; i++)
         {
-            f[i]+=f[i-1];
-            s[i]+=s[i-1];
+            firstRow[i]+=firstRow[i-1];
+            secondRow[i]+=secondRow[i-1];
         }
 
-        long long ans=LLONG_MAX, maxi=LLONG_MIN;
-
-        int blue=0, red=0;
+        long long res=LLONG_MAX;
 
         for(int i=0; i<n; i++)
-        { 
-            long long b1=f[n-1]-f[i], b2=(i-1>=0)?s[i-1]:0;
+        {
+            long long pathFollowed= i>0 ? secondRow[i-1] : 0,
+            pathToFollow=firstRow[n-1]-firstRow[i];
 
-            maxi=max(b1,b2);
-            ans=min(maxi,ans);
+            long long maxi=max(pathFollowed, pathToFollow);
+
+            res=min(maxi, res); 
         }
-        
-        return ans;
+
+        return res;
+
     }
 };
