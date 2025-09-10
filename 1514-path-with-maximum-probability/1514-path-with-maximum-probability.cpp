00ec9adfcb507fdbbd86d8a3a65478191ed81fac
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-09-10 12:53:27 +0530

    Time: 61 ms (34.43%), Space: 74.8 MB (19.07%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        // Build the graph
-        unordered_map<int, vector<pair<int, double>>> mp;
-        for (int i = 0; i < edges.size(); ++i) {
-            mp[edges[i][0]].emplace_back(edges[i][1], succProb[i]);
-            mp[edges[i][1]].emplace_back(edges[i][0], succProb[i]);
+        unordered_map<int, vector<pair<int, double>>>mp;
+
+        for(int i=0; i<edges.size(); i++)
+        {
+            mp[edges[i][0]].push_back({edges[i][1], succProb[i]});
+            mp[edges[i][1]].push_back({edges[i][0], succProb[i]});
         }
 
-        priority_queue<pair<double, int>> pq;
-        pq.push({1.0, start_node});
+        priority_queue<pair<double, int>>pq;
 
-        vector<double> maxProb(n, 0.0);
-        maxProb[start_node] = 1.0;
+        vector<double>maxProb(n, 0.0);
+
+        pq.push({1.0, start_node});
+        maxProb[start_node]=1.0;
 
         while (!pq.empty()) 
         {
-            auto [prob, node] = pq.top();
+            auto [prob, node]=pq.top();
             pq.pop();
 
-            if (node == end_node) {
+            if(node==end_node)
+            { 
                 return prob;
             }
 
-            for (auto& [neighbor, edgeProb] : mp[node]) {
-                double newProb = prob * edgeProb;
-                if (newProb > maxProb[neighbor]) {
-                    maxProb[neighbor] = newProb;
+            for(auto& [neighbor, edgeProb]: mp[node]) 
+            {
+                double newProb = prob*edgeProb;
+
+                if(newProb>maxProb[neighbor]) 
+                {
+                    maxProb[neighbor]=newProb;
                     pq.push({newProb, neighbor});
                 }
             }
-        }
+        }        
 
         return 0.0;
     }
