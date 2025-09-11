510e24d52a9385898c42f11ca2fced1e2f916755
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-09-11 22:37:57 +0530

    Time: 7 ms (70.10%) | Memory: 14.4 MB (9.23%) - LeetSync

--- 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
-    int solve(string key, int k, char ch){
-        int i=0,j=0,count=0,ans=0;
-        while(j<key.size()){
-            if(key[j]!=ch) count++;
-            if(count<=k) ans=max(ans,j-i+1);
-            else{
-                while(count>k){
-                    if(key[i]!=ch) count--;
+    int solve(string key, int k, char ch)
+    {
+        int i=0, j=0, count=0, ans=0;
+
+        while(j<key.size())
+        {
+            if(key[j]!=ch)
+            { 
+                count++;
+            }
+
+            if(count<=k)
+            { 
+                ans=max(ans,j-i+1);
+            }
+            else
+            {
+                while(count>k)
+                {
+                    if(key[i]!=ch)
+                    { 
+                        count--;
+                    }
+
                     i++;
                 }
             }
+
             j++;
         }
+
         return ans;
     }
     int maxConsecutiveAnswers(string answerKey, int k) {
-        return max(solve(answerKey,k,'T'),solve(answerKey,k,'F'));
+        return max(solve(answerKey, k, 'T'), solve(answerKey, k, 'F'));
     }
 };
