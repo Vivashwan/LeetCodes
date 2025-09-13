9be974660986556d27517768f57db7e3fd94196f
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-09-13 21:52:27 +0530

    Time: 55 ms (44.88%) | Memory: 98.1 MB (29.18%) - LeetSync

--- 2325-number-of-ways-to-select-buildings/number-of-ways-to-select-buildings.cpp
+++ 2325-number-of-ways-to-select-buildings/number-of-ways-to-select-buildings.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     long long numberOfWays(string s) {
-        int n = s.length();
+        int n=s.length();
 
-        vector<long long> count0(n+1, 0), count1(n+1, 0);
+        vector<long long>count0(n+1, 0), count1(n+1, 0);
 
         for(int i=0; i<n; i++)
         {
-            count0[i+1] = count0[i] + (s[i] == '0' ? 1 : 0);
-            count1[i+1] = count1[i] + (s[i] == '1' ? 1 : 0);
+            count0[i+1]=count0[i]+(s[i]=='0' ? 1:0);
+            count1[i+1]=count1[i]+(s[i]=='1' ? 1:0);
         }
 
-        long long res = 0;
+        long long res=0;
 
         for(int i=1; i<n-1; i++)
         {
