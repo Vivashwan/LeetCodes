de90873e282cd79fbd26d8c038f25b0333ec6363
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-09-14 18:11:16 +0530

    Time: 10 ms (52.41%) | Memory: 34.9 MB (96.86%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        int n = maze.size(), m = maze[0].size(), steps = 0;
+        int n=maze.size(), m=maze[0].size(), steps=0;
 
         queue<pair<int, int>>q;
 
         q.push({entrance[0], entrance[1]});
 
-        maze[entrance[0]][entrance[1]] = '+';
+        maze[entrance[0]][entrance[1]]='+';
 
-        vector<pair<int, int>>dirs = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+        vector<pair<int, int>>dirs={{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
 
         while(!q.empty())
         {
-            int size = q.size();
+            int size=q.size();
             steps++;
 
             for(int i=0; i<size; i++)
             {
-                auto [r, c] = q.front();
+                auto [r, c]=q.front();
                 q.pop();
 
                 for(auto [rd, cd]: dirs)
@@ -27,12 +27,12 @@ public:
 
                     if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && maze[newRow][newCol]=='.')
                     {
-                        if(newRow==0 || newCol == 0 || newRow == n-1 || newCol == m-1)
+                        if(newRow==0 || newCol==0 || newRow==n-1 || newCol==m-1)
                         {
                             return steps;
                         }
 
-                        maze[newRow][newCol] = '+';
+                        maze[newRow][newCol]='+';
                         q.push({newRow, newCol});
                     }
                 }
