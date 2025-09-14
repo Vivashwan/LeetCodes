b68d2c8f7dcf147606ad337e1d7ab4b15137b6b2
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-09-14 20:57:24 +0530

    Time: 52 ms (18.64%) | Memory: 108.5 MB (12.66%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/shifting-letters-ii.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
-        int n = s.length();
-        vector<long>prefix(n+1, 0);
+        int n=s.length();
+
+        vector<int>prefix(n+1, 0);
 
         for(auto it: shifts)
         {
-            int start=it[0], end=it[1], direction=it[2];
+            int dir=it[2]==1? 1:-1;
 
-            int value = (direction==1) ? 1:-1;
-
-            prefix[start]+=value;
-            prefix[end+1]-=value;
+            prefix[it[0]]+=dir;
+            prefix[it[1]+1]-=dir;
         }
 
-        for(int i=1; i<n; i++)
+        for(int i=1; i<=n; i++)
         {
             prefix[i]+=prefix[i-1];
         }
 
         for(int i=0; i<n; i++)
         {
-            long total=prefix[i];
-
-            total=((total % 26) + 26) % 26;
+            int total=(((prefix[i]%26)+26)%26);
 
             int newChar=(s[i]-'a'+total)%26;
 
-            s[i] = 'a'+newChar;
+            s[i]='a'+newChar;
         }
 
         return s;
