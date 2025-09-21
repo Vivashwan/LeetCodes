d4a0118e26053f63205f6f36b1aa0cab0577fd18
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-09-21 18:04:02 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (65.74%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones=0, twos=0;
+
+        for(auto it: nums)
+        {
+            ones=(ones^it) & ~twos;
+            twos=(twos^it) & ~ones;
+        }
+
+        return ones;
+    }
+};
