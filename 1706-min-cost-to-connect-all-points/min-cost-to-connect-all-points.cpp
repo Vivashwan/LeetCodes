4bafa3492b099cc76b7e06cb7117de4d60b76a5d
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-09-30 20:01:01 +0530

    Time: 198 ms (68.23%) | Memory: 62.8 MB (62.15%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,40 +1,84 @@
 class Solution {
+private:
+    vector<int>parent, rank;
+
+    int find(int x)
+    {
+        while(parent[x]!=x)
+        {
+            parent[x]=parent[parent[x]];
+            x=parent[x];
+        }
+
+        return x;
+    }
+
+    bool unite(int x, int y)
+    {
+        int px=find(x), py=find(y);
+
+        if(px==py)
+        {
+            return false;
+        }
+
+        if(rank[px]<rank[py])
+        {
+            swap(px, py);
+        }
+
+        parent[py]=px;
+
+        if(rank[px]==rank[py])
+        {
+            rank[px]++;
+        }
+
+        return true;
+    }
+
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n=points.size();
 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
+        parent.resize(n);
+        rank.resize(n, 0);
 
-        pq.push({0, 0});
-
-        vector<bool>visited(n, false);
-
-        int totalCost=0, edgesUsed=0;
-
-        while(edgesUsed<n) 
+        for(int i=0; i<n; i++)
         {
-            auto [cost, u]=pq.top();
-            pq.pop();
+            parent[i]=i;
+        }
 
-            if(visited[u])
-            { 
-                continue;
-            }
+        vector<tuple<int, int, int>>edges;
 
-            visited[u]=true;
-            totalCost+=cost;
-            edgesUsed++;
-
-            for(int v=0; v<n; v++) 
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i+1; j<n; j++)
             {
-                if(!visited[v]) 
+                int dist=abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
+
+                edges.push_back({dist, i, j});
+            }
+        }
+
+        sort(edges.begin(), edges.end());
+
+        int cost=0, edgesUsed=0;
+
+        for(auto [w, u, v]: edges)
+        {
+            if(unite(u, v))
+            {
+                cost+=w;
+                edgesUsed++;
+
+                if(edgesUsed==n-1)
                 {
-                    int dist=abs(points[u][0]-points[v][0]) + abs(points[u][1]-points[v][1]);
-                    pq.push({dist, v});
+                    break;
                 }
             }
         }
 
-        return totalCost;
+        return cost;
     }
 };
