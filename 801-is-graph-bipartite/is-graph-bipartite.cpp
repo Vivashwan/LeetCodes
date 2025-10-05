32c15e81e2161258825c2f861cf1a996468d9385
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-10-05 23:53:59 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (75.55%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,36 +1,64 @@
 class Solution {
+private:
+    vector<int>parent, rank;
+
+    int find(int x) 
+    {
+        if(parent[x]!=x)
+        {    
+            parent[x]=find(parent[x]);
+        }
+
+        return parent[x];
+    }
+
+    void unite(int x, int y) 
+    {
+        int px=find(x), py=find(y);
+
+        if(px==py)
+        { 
+            return;
+        }
+
+        if(rank[px]<rank[py])
+        { 
+            swap(px, py);
+        }
+
+        parent[py]=px;
+
+        if(rank[px]==rank[py])
+        { 
+            rank[px]++;
+        }
+    }
+    
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
 
-        vector<int>color(n, -1);
+        parent.resize(n);
+        rank.resize(n, 0);
+        iota(parent.begin(), parent.end(), 0);
 
-        for(int i=0; i<n; i++)
+        for(int u=0; u<n; u++) 
         {
-            if(color[i]==-1)
+            if(graph[u].empty())
+            { 
+                continue;
+            }
+
+            int firstNeighbor=graph[u][0];
+
+            for(int v: graph[u]) 
             {
-                color[i]=0;
-                queue<int>q;
-                q.push(i);
-
-                while(!q.empty())
-                {
-                    auto u=q.front();
-                    q.pop();
-
-                    for(auto v: graph[u])
-                    {
-                        if(color[v]==-1)
-                        {
-                            color[v]=1-color[u];
-                            q.push(v);
-                        }
-                        else if(color[v]==color[u])
-                        {
-                            return false;
-                        }
-                    }
+                if(find(u)==find(v))
+                {    
+                    return false;
                 }
+
+                unite(firstNeighbor, v);
             }
         }
 
