b76b295a4e6c4fdb639a6f8d0fc8c68524a70acd
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-10-08 20:01:14 +0530

    Time: 3 ms (67.71%) | Memory: 37.3 MB (51.69%) - LeetSync

--- 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.cpp
+++ 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.cpp
@@ -1,67 +1,118 @@
 class Solution {
+private:
+    vector<int>parent, size;
+
+    int find(int x) 
+    {
+        while(parent[x]!=x) 
+        {
+            parent[x]=parent[parent[x]];
+            x=parent[x];
+        }
+
+        return x;
+    }
+
+    void unite(int x, int y) 
+    {
+        int px=find(x), py=find(y);
+
+        if(px==py)
+        { 
+            return;
+        }
+        if(size[px]>size[py])
+        { 
+            parent[py]=px;
+        }
+        else if(size[px]<size[py])
+        { 
+            parent[px]=py;
+        }
+        else 
+        { 
+            parent[py]=px; 
+            size[px]+=size[py];
+        }
+    }
+
 public:
     bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
         vector<int>indegree(n, 0);
+        
+        parent.resize(n);
+        size.assign(n, 1);
 
-        for(int i=0; i<n; i++)
+        iota(parent.begin(), parent.end(), 0);
+
+        for(int i=0; i<n; i++) 
         {
             if(leftChild[i]!=-1)
             { 
                 indegree[leftChild[i]]++;
             }
 
-            if(rightChild[i]!=-1)
-            { 
+            if(rightChild[i]!=-1) 
+            {
                 indegree[rightChild[i]]++;
             }
         }
 
-        int root=-1;
-
-        for(int i=0; i<n; i++)
+        for(int i=0; i<n; i++) 
         {
-            if(indegree[i]==0)
-            {
-                if(root==-1)
-                { 
-                    root=i;
-                }
-                else return false;
+            if(indegree[i]>1)
+            { 
+                return false;
             }
         }
 
-        if(root==-1)
+        int rootCount=0;
+
+        for(int i=0; i<n; i++) 
+        {
+            if(indegree[i]==0)
+            { 
+                rootCount++;
+            }
+        }
+
+        if(rootCount!=1) 
         {
             return false;
         }
 
-        vector<bool>visited(n, false);
-        queue<int>q;
-        q.push(root);
-
-        while(!q.empty())
+        for(int i=0; i<n; i++) 
         {
-            int node=q.front();
-            q.pop();
-
-            if(visited[node])
+            if(leftChild[i]!=-1) 
             {
-                return false;
+                if(find(i)==find(leftChild[i])) 
+                {
+                    return false;
+                }
+                unite(i, leftChild[i]);
             }
 
-            visited[node]=true;
-
-            if(leftChild[node]!=-1)
+            if(rightChild[i]!=-1) 
             {
-                q.push(leftChild[node]);
-            }
+                if(find(i)==find(rightChild[i]))
+                { 
+                    return false;
+                }
 
-            if(rightChild[node]!=-1)
-            {
-                q.push(rightChild[node]);
+                unite(i, rightChild[i]);
             }
         }
 
-        return accumulate(visited.begin(), visited.end(), 0)==n;
+        int root=find(0);
+
+        for(int i=1; i<n; i++) 
+        {
+            if(find(i)!=root)
+            { 
+                return false;
+            }
+        }
+
+        return true;
     }
 };
