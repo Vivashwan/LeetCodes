0365e787cee39f7f7ba42a1e1f277d0553678988
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-10-10 23:41:06 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (75.58%) - LeetSync

--- 999-regions-cut-by-slashes/regions-cut-by-slashes.cpp
+++ 999-regions-cut-by-slashes/regions-cut-by-slashes.cpp
@@ -1,62 +1,96 @@
 class Solution {
-public:
-    void dfs(vector<vector<int>>&grid, int i, int j, int n) 
+private:
+    vector<int>parent, size;
+
+    int find(int x)
     {
-        if(i<0 || j<0 || i>=n || j>=n || grid[i][j]==1)
-        {    
+        while(parent[x]!=x)
+        {
+            parent[x]=parent[parent[x]];
+            x=parent[x];
+        }
+
+        return x;
+    }
+
+    void unite(int x, int y)
+    {
+        int px=find(x), py=find(y);
+
+        if(px==py)
+        {
             return;
         }
-        
-        grid[i][j]=1;
-        
-        dfs(grid, i+1, j, n);
-        dfs(grid, i-1, j, n);
-        dfs(grid, i, j+1, n);
-        dfs(grid, i, j-1, n);
+        else if(size[px]>size[py])
+        {
+            parent[py]=px;
+        }
+        else if(size[px]<size[py])
+        {
+            parent[px]=py;
+        }
+        else
+        {
+            parent[py]=px;
+            size[px]++;
+        }
     }
-    
+
+public:
     int regionsBySlashes(vector<string>& grid) {
         int n=grid.size();
-        int expandedN=n*3;
-        
-        vector<vector<int>>expandedGrid(expandedN, vector<int>(expandedN, 0));
-        
-        for(int i=0; i<n; i++) 
-        {
-            for(int j=0; j<n; j++) 
-            {
-                char c=grid[i][j];
-                int r=i*3, c3=j*3;
-                
-                if(c=='/') 
-                {
-                    expandedGrid[r][c3+2]=1;
-                    expandedGrid[r+1][c3+1]=1;
-                    expandedGrid[r+2][c3+0]=1;
-                } 
-                else if (c=='\\') 
-                {
-                    expandedGrid[r][c3+0]=1;
-                    expandedGrid[r+1][c3+1]=1;
-                    expandedGrid[r+2][c3+2]=1;
-                }
-            }
-        }
-        
-        int regions=0;
 
-        for(int i=0; i<expandedN; i++) 
+        parent.resize(4*n*n);
+        size.resize(4*n*n, 1);
+
+        iota(parent.begin(), parent.end(), 0);
+
+        for(int i=0; i<n; i++)
         {
-            for(int j=0; j<expandedN; j++) 
+            for(int j=0; j<n; j++)
             {
-                if(expandedGrid[i][j]==0) 
+                int id=4*(i*n+j);
+
+                if(grid[i][j]=='/')
                 {
-                    dfs(expandedGrid, i, j, expandedN);
-                    regions++;
+                    unite(id, id+3);
+                    unite(id+1, id+2);
+                }
+                else if(grid[i][j]=='\\')
+                {
+                    unite(id, id+1);
+                    unite(id+2, id+3);
+                }
+                else
+                {
+                    unite(id, id+1);
+                    unite(id+1, id+2);
+                    unite(id+2, id+3);
+                    unite(id, id+3);
+                }
+
+                if(j+1<n)
+                {
+                    unite(4*(i*n+j)+1, 4*(i*n+j+1)+3);
+                }
+
+                if(i+1<n)
+                {
+                    unite(4*(i*n+j)+2, 4*((i+1)*n+j)+0);
                 }
             }
         }
-        
-        return regions;
+
+        int count=0;
+
+        for(int i=0; i<4*n*n; i++)
+        {
+            if(find(i)==i)
+            {
+                count++;
+            }
+        }
+
+        return count;
     }
 };
