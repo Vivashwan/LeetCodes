eda656bdbe5195daea0d575c2207484696b2218f
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-10-24 01:03:32 +0530

    Time: 8 ms (91.52%) | Memory: 16.7 MB (18.86%) - LeetSync

--- 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
@@ -1,10 +1,77 @@
+class SegmentTree{
+    vector<int>tree;
+    int n;
+public:
+    SegmentTree(int size)
+    {
+        n=size;
+        tree.resize(4*n, 0);
+
+        build(0, 0, n-1);
+    }
+
+    void build(int node, int start, int end)
+    {
+        if(start==end)
+        {
+            tree[node]=1;
+            return;
+        }
+
+        int mid=(start+end)/2;
+
+        build(2*node+1, start, mid);
+        build(2*node+2, mid+1, end);
+
+        tree[node]=tree[2*node+1]+tree[2*node+2];
+    }
+
+    int query(int node, int start, int end, int k)
+    {
+        if(start==end)
+        {
+            return start;
+        }
+
+        int mid=(start+end)/2;
+        int left=tree[2*node+1];
+
+        if(k<=left)
+        {
+            return query(2*node+1, start, mid, k);
+        }
+        else return query(2*node+2, mid+1, end, k-left);
+    }
+
+    void update(int node, int start, int end, int ind)
+    {
+        if(start==end)
+        {
+            tree[node]=0;
+            return;
+        }
+
+        int mid=(start+end)/2;
+
+        if(ind<=mid)
+        {
+            update(2*node+1, start, mid, ind);
+        }
+        else
+        {
+            update(2*node+2, mid+1, end, ind);
+        }
+
+        tree[node]=tree[2*node+1]+tree[2*node+2];
+    }
+};
 class Solution {
 private:
     static bool myCmp(vector<int>&a, vector<int>&b)
     {
         if(a[0]==b[0])
-        {
-            return a[1]<b[1];
+        { 
+            return a[1]>b[1];
         }
 
         return a[0]<b[0];
@@ -15,26 +82,15 @@ public:
 
         sort(people.begin(), people.end(), myCmp);
 
-        vector<vector<int>>res(n, vector<int>(2, -1));
+        vector<vector<int>>res(n, vector<int>(2, 0));
 
-        for(int i=0; i<n; i++)
+        SegmentTree seg(n);
+
+        for(auto it: people)
         {
-            int count=people[i][1];
-
-            for(int j=0; j<n; j++)
-            {
-                if(res[j][0]==-1 && count==0)
-                {
-                    res[j][0]=people[i][0];
-                    res[j][1]=people[i][1];
-
-                    break;
-                }
-                else if(res[j][0]==-1 || (res[j][0]>=people[i][0]))
-                {
-                    count--;
-                }
-            }
+            int ind=seg.query(0, 0, n-1, it[1]+1);
+            res[ind]=it;
+            seg.update(0, 0, n-1, ind);
         }
 
         return res;
