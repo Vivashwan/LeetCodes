484a8396c2260c3c36fc246560d471f95d959179
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-10-29 01:43:22 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (75.99%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -1,34 +1,30 @@
 class Solution {
 private:
-    void func(int u, int target, vector<int>temp, vector<vector<int>>&res, vector<vector<int>>&graph)
+    void dfs(vector<vector<int>>&adj, vector<int>&temp, vector<vector<int>>& res, int ind, int n)
     {
-        temp.push_back(u);
-
-        if(u==target)
+        if(ind==n-1)
         {
             res.push_back(temp);
             return;
         }
 
-        for(auto v: graph[u])
+        for(auto it: adj[ind])
         {
-            func(v, target, temp, res, graph);
+            temp.push_back(it);
+            dfs(adj, temp, res, it, n);
+            temp.pop_back();
         }
     }
+
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         int n=graph.size();
 
         vector<vector<int>>res;
+        vector<int>temp;
+        temp.push_back(0);
 
-        for(auto u: graph[0])
-        {
-            vector<int>temp;
-
-            temp.push_back(0);
-
-            func(u, n-1, temp, res, graph);
-        }
+        dfs(graph, temp, res, 0, n);
 
         return res;
     }
