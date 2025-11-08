a847aebc777d413efd47a60a80efee40a27d1145
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-11-08 22:58:45 +0530

    Time: 202 ms (72.68%) | Memory: 47.1 MB (52.68%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,28 +1,30 @@
 class Solution {
-private:
-    int solve(int ind, string& s, unordered_set<string>& st, string temp){
-        if(ind == s.length())
-            return 0;
-        else{
-            temp.push_back(s[ind]);
-            int op1 = -1;
-            int op2 = -1;
-			// if new substring is not present in the set
-            if(st.find(temp) == st.end()){
-                st.insert(temp);
-                op1 = 1 + solve(ind+1, s, st, "");  //since new substring is added we increase the cnt by 1 and create a new substring to check from very next index -----> in other words we pick that string
-                st.erase(temp);
+public:
+    void dfs(string &s, int start, unordered_set<string>&used, int count, int &best) 
+    {
+        if(start==s.size()) 
+        {
+            best=max(best, count);
+            return;
+        }
+
+        for(int end=start; end<s.size(); ++end) 
+        {
+            string sub=s.substr(start, end-start+1);
+
+            if(used.find(sub)==used.end()) 
+            {
+                used.insert(sub);
+                dfs(s, end+1, used, count+1, best);
+                used.erase(sub);
             }
-			// picked part was done,  its time for not picking the char at that index and moving to the next one
-            op2 = solve(ind+1, s, st, temp);
-            return max(op1, op2);
         }
     }
-public:
+
     int maxUniqueSplit(string s) {
-        // creating unordered_set to keep track which substring is visited
-        unordered_set<string> st;
-		// recursion function
-        return solve(0, s, st, ""); 
+        int best=0;
+        unordered_set<string>used;
+        dfs(s, 0, used, 0, best);
+        return best;
     }
 };
