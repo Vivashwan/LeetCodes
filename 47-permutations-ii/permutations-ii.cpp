3a6b26eb441452b0bcdd4cb79022aa5473c04b4d
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-11-09 22:03:26 +0530

    Time: 8 ms (32.35%) | Memory: 15.8 MB (11.64%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,37 +1,37 @@
 class Solution {
 private:
-    void func(int ind, vector<int>&nums, int n, vector<vector<int>>&res)
+    vector<vector<int>>res;
+
+    void func(int ind, int n, vector<int>&nums)
     {
-        if(ind==n-1)
+        if(ind==n)
         {
             res.push_back(nums);
             return;
         }
 
-        unordered_set<int>st;
+        unordered_set<int>s;
 
         for(int i=ind; i<n; i++)
         {
-            if(st.count(nums[i]))
+            if(s.count(nums[i]))
             {
                 continue;
             }
 
-            st.insert(nums[i]);
-
+            s.insert(nums[i]);
+            
             swap(nums[ind], nums[i]);
-            func(ind+1, nums, n, res);
-            swap(nums[i], nums[ind]);
-
+            func(ind+1, n, nums);
+            swap(nums[ind], nums[i]);
         }
     }
+    
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>>res;
-
         int n=nums.size();
 
-        func(0, nums, n, res);
+        func(0, n, nums);
 
         return res;
     }
