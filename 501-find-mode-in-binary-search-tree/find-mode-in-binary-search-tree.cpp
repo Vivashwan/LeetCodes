77627950ee5e32a6ad0ffcf8d6e39dc48da18e59
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-11-19 23:55:01 +0530

    Time: 3 ms (55.73%) | Memory: 25.6 MB (47.26%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.cpp
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.cpp
@@ -1,52 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-private:
-    int currentVal;
-    int currentCount;
-    int maxCount;
-    vector<int> modes;
-
-    void inorder(TreeNode* node) {
-        if (!node) return;
-
-        inorder(node->left);
-
-        if (node->val == currentVal) {
-            currentCount++;
-        } else {
-            currentVal = node->val;
-            currentCount = 1;
+public:
+    void func(TreeNode* root, unordered_map<int,int>&mp, int& mx)
+    {
+        if(!root)
+        { 
+            return;
         }
 
-        if (currentCount == maxCount) {
-            modes.push_back(currentVal);
-        } else if (currentCount > maxCount) {
-            modes.clear();
-            modes.push_back(currentVal);
-            maxCount = currentCount;
-        }
+        func(root->left, mp, mx);
 
-        inorder(node->right);
+        mp[root->val]++;
+        mx=max(mx, mp[root->val]);
+
+        func(root->right, mp, mx);
     }
 
-public:
     vector<int> findMode(TreeNode* root) {
-        currentVal = INT_MIN;
-        currentCount = 0;
-        maxCount = 0;
-        modes.clear();
+        unordered_map<int,int> mp;
+        int mx=0;
 
-        inorder(root);
-        return modes;
+        func(root, mp, mx);
+
+        vector<int>res;
+
+        for(auto &p: mp) 
+        {
+            if(p.second==mx)
+            { 
+                res.push_back(p.first);
+            }
+        }
+
+        return res;
     }
 };
