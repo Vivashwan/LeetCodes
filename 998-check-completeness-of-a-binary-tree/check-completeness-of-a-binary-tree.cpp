5da3c61f9347913b891345b490699170a26366e0
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-11-22 22:39:02 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (57.35%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
@@ -12,34 +12,30 @@
 class Solution {
 public:
     bool isCompleteTree(TreeNode* root) {
-        if(!root)
-        { 
-            return true;
-        }
-
         queue<TreeNode*>q;
+
         q.push(root);
 
         bool end=false;
 
         while(!q.empty())
         {
-            TreeNode* current=q.front();
+            auto it=q.front();
             q.pop();
 
-            if(!current)
+            if(!it)
             {
                 end=true;
-            } 
-            else 
+            }
+            else
             {
                 if(end)
-                { 
+                {
                     return false;
                 }
 
-                q.push(current->left);
-                q.push(current->right);
+                q.push(it->left);
+                q.push(it->right);
             }
         }
 
