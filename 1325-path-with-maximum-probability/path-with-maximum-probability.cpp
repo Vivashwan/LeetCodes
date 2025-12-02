906be465fbcaec137279ae3c0fb37054ca2c25ea
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-12-02 09:02:59 +0530

    Time: 75 ms (15.98%) | Memory: 74.7 MB (21.25%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -11,32 +11,33 @@ public:
 
         priority_queue<pair<double, int>>pq;
 
+        pq.push({1.0, start_node});
+
         vector<double>maxProb(n, 0.0);
 
-        pq.push({1.0, start_node});
         maxProb[start_node]=1.0;
 
-        while (!pq.empty()) 
+        while(!pq.empty())
         {
             auto [prob, node]=pq.top();
             pq.pop();
 
             if(node==end_node)
-            { 
+            {
                 return prob;
             }
 
-            for(auto& [neighbor, edgeProb]: mp[node]) 
+            for(auto [neighbor, edgeProb]: mp[node])
             {
-                double newProb = prob*edgeProb;
+                double newProb=prob*edgeProb;
 
-                if(newProb>maxProb[neighbor]) 
+                if(newProb>maxProb[neighbor])
                 {
                     maxProb[neighbor]=newProb;
                     pq.push({newProb, neighbor});
                 }
             }
-        }        
+        }
 
         return 0.0;
     }
