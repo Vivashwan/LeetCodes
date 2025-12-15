a65cd2c72f37615e21bfa00e67c26ca29ad85af0
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-12-15 18:17:07 +0530

    Time: 352 ms (38.70%) | Memory: 109 MB (13.39%) - LeetSync

--- 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
@@ -1,52 +1,41 @@
 class Solution {
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
-        int n = classes.size();
-        priority_queue<pair<double, int>>pq;
+        priority_queue<pair<double, pair<int, int>>>pq;
 
-        for(int i=0; i<n; i++)
+        for(auto it: classes)
         {
-            int pass = classes[i][0], total = classes[i][1];
+            int passing=it[0], total=it[1];
 
-            double ratio1 = (double)pass/(double)total;
+            double gain=(double)(passing+1)/(total+1)-(double)passing/total;
 
-            int newPass = classes[i][0]+1, newTotal = classes[i][1]+1;
-
-            double ratio2 = (double)newPass/(double)newTotal;
-
-            pq.push({ratio2-ratio1, i});
+            pq.push({gain, {passing, total}});
         }
 
-        while(extraStudents)
+        while(extraStudents--)
         {
-            auto it = pq.top();
+            auto it=pq.top();
             pq.pop();
 
-            int index = it.second;
+            int passing=it.second.first, total=it.second.second;
 
-            classes[index][0]+=1;
-            classes[index][1]+=1;
+            passing++, total++;
 
-            int pass = classes[index][0], total = classes[index][1];
+            double newGain=(double)(passing+1)/(total+1)-(double)passing/total;
 
-            double ratio1 = double(pass)/(double)total;
-
-            int newPass = classes[index][0]+1, newTotal = classes[index][1]+1;
-
-            double ratio2 = (double)newPass/(double)newTotal;
-
-            pq.push({ratio2-ratio1, index});
-
-            extraStudents--;
+            pq.push({newGain, {passing, total}});
         }
 
         double sum=0;
 
-        for(auto it:classes)
+        while(!pq.empty())
         {
-            sum+=(double)it[0]/(double)it[1];
+            auto it=pq.top();
+            pq.pop();
+
+            sum+=(double)it.second.first/it.second.second;
         }
 
-        return sum/(double)n;
+        return sum/classes.size();
     }
 };
