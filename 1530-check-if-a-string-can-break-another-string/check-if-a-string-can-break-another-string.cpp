cafeb2f269fa852ca6a5d9c79fef988a7eca5bbc
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-12-18 21:52:53 +0530

    Time: 30 ms (70.11%) | Memory: 16.9 MB (5.69%) - LeetSync

--- 1530-check-if-a-string-can-break-another-string/check-if-a-string-can-break-another-string.cpp
+++ 1530-check-if-a-string-can-break-another-string/check-if-a-string-can-break-another-string.cpp
@@ -1,33 +1,22 @@
 class Solution {
+private:
+    bool func(string s1, string s2)
+    {
+        for(int i=0; i<s1.length(); i++)
+        {
+            if(s1[i]<s2[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 public:
     bool checkIfCanBreak(string s1, string s2) {
-        int n=s1.size();
-        
         sort(s1.begin(), s1.end());
         sort(s2.begin(), s2.end());
 
-        bool flag1=true, flag2=true;
-
-        for(int i=0; i<n; i++)
-        {
-            if(s1[i]>=s2[i])
-            {
-                continue;
-            }
-
-            flag1=false;
-        }
-
-        for(int i=0; i<n; i++)
-        {
-            if(s2[i]>=s1[i])
-            {
-                continue;
-            }
-
-            flag2=false;
-        }
-
-        return flag1 || flag2;
+        return func(s1, s2) || func(s2, s1);
     }
 };
