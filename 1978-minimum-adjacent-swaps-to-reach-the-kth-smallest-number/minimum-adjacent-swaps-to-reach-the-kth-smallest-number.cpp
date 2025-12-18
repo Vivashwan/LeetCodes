748c92e99ccfdc080a032325c7feb4b54e4e2367
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-12-18 20:25:49 +0530

    Time: 25 ms (28.15%) | Memory: 9.4 MB (21.48%) - LeetSync

--- 1978-minimum-adjacent-swaps-to-reach-the-kth-smallest-number/minimum-adjacent-swaps-to-reach-the-kth-smallest-number.cpp
+++ 1978-minimum-adjacent-swaps-to-reach-the-kth-smallest-number/minimum-adjacent-swaps-to-reach-the-kth-smallest-number.cpp
@@ -1,26 +1,64 @@
 class Solution {
+private:
+    void nextPermutation(string &target)
+    {
+        int n=target.length();
+
+        int i=n-2;
+
+        while(i>=0 && target[i]>=target[i+1])
+        {
+            i--;
+        }
+
+        if(i<0) 
+        {
+            sort(target.begin(), target.end());
+            return;
+        }
+
+        int j=n-1;
+
+        while(j>=i && target[j]<=target[i])
+        {
+            j--;
+        }
+
+        swap(target[i], target[j]);
+
+        reverse(target.begin()+i+1, target.end());
+    }
 public:
     int getMinSwaps(string num, int k) {
-        int n = num.length();
-
-        string temp = num;
+        string target=num;
+        int n=num.length();
 
         while(k--)
         {
-            next_permutation(temp.begin(), temp.end());
+            nextPermutation(target);
         }
 
-        int res = 0;
+        int res=0;
 
         for(int i=0; i<n; i++)
         {
-            int j=i+1;
-
-            while(num[i]!=temp[i])
+            if(target[i]==num[i])
             {
-                res++;
-                swap(temp[i], temp[j]);
+                continue;
+            }
+
+            int j=i;
+
+            while(j<n && target[i]!=num[j])
+            {
                 j++;
+            }
+
+            while(j>i)
+            {
+                swap(num[j], num[j-1]);
+                j--;
+                res++;
             }
         }
 
