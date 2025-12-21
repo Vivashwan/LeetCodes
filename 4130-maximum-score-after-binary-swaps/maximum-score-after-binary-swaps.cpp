473b3379ec83b31b0c77028e9369964cdefd6a44
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-12-21 15:47:37 +0530

    Time: 55 ms (66.67%) | Memory: 131.6 MB (33.33%) - LeetSync

---
+++ 4130-maximum-score-after-binary-swaps/maximum-score-after-binary-swaps.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long maximumScore(vector<int>& nums, string s) {
+        long long n=nums.size();
+
+        priority_queue<long long>pq;
+
+        long long i=0, ans=0;
+
+        while(i<n)
+        {
+            pq.push(nums[i]);
+
+            if(s[i]=='1')
+            {
+                ans+=pq.top();
+                pq.pop();
+            }
+
+            i++;
+        }
+
+        return ans;
+    }
+};
