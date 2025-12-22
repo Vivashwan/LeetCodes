2fcea3c1cc5f6fd6834a5c12cc62c653159b3351
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-12-22 20:26:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (80.05%) - LeetSync

--- 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
+++ 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     int mctFromLeafValues(vector<int>& arr) {
+        int n=arr.size();
+
         int res=0;
+
         stack<int>st;
 
         st.push(INT_MAX);
 
-        for(auto it: arr)
+        for(int i=0; i<n; i++)
         {
-            while(!st.empty() && st.top()<=it)
+            while(!st.empty() && st.top()<=arr[i])
             {
                 int mid=st.top();
                 st.pop();
-                res+=mid*min(st.top(), it);
+
+                res+=min(st.top(), arr[i])*mid;
             }
-            
-            st.push(it);
+
+            st.push(arr[i]);
         }
 
         while(st.size()>2)
         {
             int mid=st.top();
             st.pop();
+
             res+=mid*st.top();
         }
 
