3c3a254fc8fde135987404315e51a308702eb67f
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-12-24 15:06:54 +0530

    Time: 20 ms (5.78%) | Memory: 55.2 MB (68.23%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -3,22 +3,22 @@ public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
         sort(arr.begin(), arr.end());
 
-        int n = arr.size();
+        int n=arr.size();
 
-        arr[0] = 1;
+        arr[0]=1;
 
-        int maxim = arr[0];
+        int maxim=arr[0];
 
         for(int i=1; i<n; i++)
         {
             if(abs(arr[i]-arr[i-1])>1)
             {
-                arr[i] = arr[i-1]+1;
-                maxim = max(maxim, arr[i]);
+                arr[i]=arr[i-1]+1;
+                maxim=max(maxim, arr[i]);
             }
             else
             {
-                maxim = max(maxim, arr[i]);
+                maxim=max(maxim, arr[i]);
             }
         }
 
