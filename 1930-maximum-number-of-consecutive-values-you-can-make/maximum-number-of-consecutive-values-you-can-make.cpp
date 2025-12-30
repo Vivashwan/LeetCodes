78b0a6a896f9078e71dbf964dcdf6a0c2c703ac5
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-12-30 17:54:48 +0530

    Time: 22 ms (51.33%) | Memory: 70.3 MB (84.00%) - LeetSync

--- 1930-maximum-number-of-consecutive-values-you-can-make/maximum-number-of-consecutive-values-you-can-make.cpp
+++ 1930-maximum-number-of-consecutive-values-you-can-make/maximum-number-of-consecutive-values-you-can-make.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int getMaximumConsecutive(vector<int>& coins) {
-        int res = 1;
+        int res=1;
         sort(begin(coins), end(coins));
-        for (int coin: coins) {
-            if (res >= coin) res += coin;
+
+        for(int coin: coins) 
+        {
+            if(res>=coin)
+            { 
+                res+=coin;
+            }
             else break;
         }
+        
         return res;
     }
 };
