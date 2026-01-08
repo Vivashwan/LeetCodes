d93b3226c16fcb96dabef290256f8e118647a706
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2026-01-08 16:13:53 +0530

    Time: 3 ms (15.54%) | Memory: 8.8 MB (7.23%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,54 +1,45 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        unordered_map<char, int>mp;
-        priority_queue<pair<int, char>>pq;
+        unordered_map<char, int> mp;
 
-        for(auto it: s)
+        for (char c : s) mp[c]++;
+
+        priority_queue<pair<int, char>> pq;
+        for (auto it : mp) pq.push({it.second, it.first});
+
+        string res = "";
+
+        while (pq.size() > 1)
         {
-            mp[it]++;
-        }
+            vector<pair<int, char>> temp;
 
-        for(auto it: mp)
-        {
-            pq.push({it.second, it.first});
-        }
-
-        string res="";
-
-        while(pq.size()>1)
-        {
-            auto u=pq.top(); pq.pop();
-            auto v=pq.top(); pq.pop();
-
-            res+=u.second; res+=v.second;
-
-            u.first--, v.first--;
-
-            if(u.first>0)
+            for (int i = 0; i < 2; i++)
             {
-                pq.push(u);
-            }
-
-            if(v.first>0)
-            {
-                pq.push(v);
-            }
-        }
-
-        if(!pq.empty())
-        {
-            if(pq.top().first>1)
-            {
-                return "";
-            }
-            else 
-            {
-                res+=pq.top().second;
+                auto it = pq.top();
                 pq.pop();
+
+                res += it.second;
+                it.first--;
+
+                temp.push_back(it);
+            }
+
+            for (auto it : temp)
+            {
+                if (it.first > 0)
+                    pq.push(it);
             }
         }
 
-        return res;
+        // Case 1: nothing left
+        if (pq.empty())
+            return res;
+
+        // Case 2: one char left
+        if (pq.top().first > 1)
+            return "";
+
+        return res + pq.top().second;
     }
 };
