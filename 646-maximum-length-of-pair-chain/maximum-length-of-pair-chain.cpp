dc46a8d2d704a9235d5053750c77d5281c1c367a
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2026-01-10 21:30:29 +0530

    Time: 0 ms (100.00%) | Memory: 26.6 MB (58.83%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -1,39 +1,25 @@
 class Solution {
 private:
-    static bool myCmp(const vector<int>&a, const vector<int>&b)
+    static bool myCmp(vector<int>&a, vector<int>&b) 
     {
-        return a[0]<b[0];
-    }
-
-    int func(vector<vector<int>>&pairs, int n, vector<vector<int>>&dp)
-    {
-        for(int ind=n-1; ind>=0; ind--)
-        {
-            for(int prev=ind-1; prev>=-1; prev--)
-            {
-                int pick=0;
-
-                if(prev==-1 || pairs[prev][1]<pairs[ind][0])
-                {
-                    pick = 1+dp[ind+1][ind+1];
-                }
-
-                int notPick = 0+dp[ind+1][prev+1];
-
-                dp[ind][prev+1] = max(pick, notPick);
-            }
-        }
-
-        return dp[0][0];
+        return a[1]<b[1];
     }
 public:
     int findLongestChain(vector<vector<int>>& pairs) {
-        int n = pairs.size();
-
+        int n=pairs.size();
         sort(pairs.begin(), pairs.end(), myCmp);
 
-        vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
+        int count=1, prevEnd=pairs[0][1];
 
-        return func(pairs, n, dp);
+        for(int i=1; i<n; i++) 
+        {
+            if(pairs[i][0]>prevEnd) 
+            {
+                count++;
+                prevEnd=pairs[i][1];
+            }
+        }
+
+        return count;
     }
 };
