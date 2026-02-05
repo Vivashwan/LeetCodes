87045432ad3a64f41e15b45af69b49c579f12c62
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2026-02-05 18:12:50 +0530

    Time: 9 ms (36.20%) | Memory: 16.9 MB (87.73%) - LeetSync

---
+++ 2817-minimum-cost-to-make-all-characters-equal/minimum-cost-to-make-all-characters-equal.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long minimumCost(string s) {
+        int n=s.size();
+        long long cost=0;
+
+        for(int i=1; i<n; i++) 
+        {
+            if(s[i]!=s[i-1]) 
+            {
+                cost+=min(i, n-i);
+            }
+        }
+
+        return cost;
+    }
+};
