706abe6d51d8f8c797acee26d65035a40ec8360f
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2026-03-03 23:03:48 +0530

    Time: 15 ms (52.99%) | Memory: 75.1 MB (42.73%) - LeetSync

--- 2530-minimize-maximum-of-array/minimize-maximum-of-array.cpp
+++ 2530-minimize-maximum-of-array/minimize-maximum-of-array.cpp
@@ -1,15 +1,42 @@
 class Solution {
-public:
-    int minimizeArrayValue(vector<int>& nums) {
-        int n = nums.size();
-
-        long long total = 0, res = 0;
+private:
+    bool canMake(vector<int>&nums, int n, long long mid)
+    {
+        long long total=0;
 
         for(int i=0; i<n; i++)
         {
-            total += nums[i];
-            long long val=ceil((total)/(i+1.0));
-            res = max(res, val);
+            total+=nums[i];
+
+            if(total>mid*(i+1))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+public:
+    int minimizeArrayValue(vector<int>& nums) {
+        int n=nums.size();
+
+        int low=0, high=*max_element(nums.begin(), nums.end());
+
+        int res=high;
+
+        while(low<=high)
+        {
+            long long mid=low+(high-low)/2;
+
+            if(canMake(nums, n, mid))
+            {
+                res=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
         }
 
         return res;
