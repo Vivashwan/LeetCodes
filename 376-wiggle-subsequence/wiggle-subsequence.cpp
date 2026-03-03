b99eaf7cbb9cd895183c7cf38a1d4c7738669ce7
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2026-03-03 18:30:14 +0530

    Time: 252 ms (7.15%) | Memory: 100.3 MB (5.22%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/wiggle-subsequence.cpp
@@ -1,35 +1,38 @@
 class Solution {
+private:
+    int func(vector<int>&nums, int ind, int prev, bool positive, vector<vector<vector<int>>>&dp)
+    {
+        if(ind==nums.size())
+        {
+            return 0;
+        }
+
+        if(dp[ind][prev][positive]!=-1)
+        {
+            return dp[ind][prev][positive];
+        }
+
+        int notPick=func(nums, ind+1, prev, positive, dp);
+        int pick=0;
+
+        if((positive && nums[ind]>nums[prev]) || (!positive && nums[ind]<nums[prev]))
+        {
+            pick=1+func(nums, ind+1, ind, !positive, dp);
+        }
+
+        return dp[ind][prev][positive]=max(pick, notPick);
+    }
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        int n = nums.size();
-
-        if(n==1) 
+        int n=nums.size();
+        if(n==1)
         {
             return 1;
         }
 
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(n+1, vector<int>(2, 0)));
+        vector<vector<vector<int>>>dp;
+        dp.resize(n+1, vector<vector<int>>(n+1, vector<int>(2, -1)));
 
-        for(int ind=n-1; ind>=0; ind--) 
-        {
-            for(int prev=ind-1; prev>=-1; prev--) 
-            {  
-                for(int positive=0; positive<2; positive++) 
-                {
-                    int pick=0;
-
-                    if((positive && (prev==-1 || nums[ind]>nums[prev])) || 
-                        (!positive && (prev==-1 || nums[ind]<nums[prev]))) {
-                        pick = 1 + dp[ind+1][ind+1][!positive]; 
-                    }
-
-                    int notPick = (ind+1<n) ? dp[ind+1][prev+1][positive]:0;
-                    
-                    dp[ind][prev+1][positive] = max(pick, notPick);
-                }
-            }
-        }
-        
-        return max(dp[0][0][0], dp[0][0][1]);
+        return 1+max(func(nums, 1, 0, true, dp), func(nums, 1, 0, false, dp));
     }
 };
