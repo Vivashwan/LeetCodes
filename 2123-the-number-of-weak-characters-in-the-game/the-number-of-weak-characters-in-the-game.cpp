af0083843905b63760ba533b4ed9076359f2430d
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2026-03-04 22:43:28 +0530

    Time: 86 ms (48.19%) | Memory: 129.2 MB (86.02%) - LeetSync

--- 2123-the-number-of-weak-characters-in-the-game/the-number-of-weak-characters-in-the-game.cpp
+++ 2123-the-number-of-weak-characters-in-the-game/the-number-of-weak-characters-in-the-game.cpp
@@ -11,11 +11,11 @@ private:
     }
 public:
     int numberOfWeakCharacters(vector<vector<int>>& properties) {
-        int n = properties.size();
+        int n=properties.size();
 
         sort(properties.begin(), properties.end(), myCmp);
 
-        int count = 0, maxi = INT_MIN;
+        int count=0, maxi=INT_MIN;
 
         for(int i=n-1; i>=0; i--)
         {
@@ -24,9 +24,9 @@ public:
                 count++;
             }
 
-            maxi = max(maxi, properties[i][1]);
+            maxi=max(maxi, properties[i][1]);
         }
-        
+
         return count;
     }
 };
