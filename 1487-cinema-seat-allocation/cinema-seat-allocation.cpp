c0504d8a1bdd88b5785cb9d44552a2c4da5a87f7
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2026-03-05 15:26:55 +0530

    Time: 98 ms (25.68%) | Memory: 83.8 MB (23.46%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -3,58 +3,58 @@ public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
         unordered_map<int, vector<int>>mp;
 
-        for(auto it: reservedSeats) 
+        for(auto it: reservedSeats)
         {
             mp[it[0]].push_back(it[1]);
         }
 
-        int ans = 2*(n-mp.size());  
+        int res=2*(n-mp.size());
 
-        for(auto it: mp) 
+        for(auto it: mp)
         {
-            auto seats = it.second;
+            auto seats=it.second;
 
-            bool isLeftAisle=false, isRightAisle=false, isMiddle=false;
+            bool isLeftBlocked=false, isRightBlocked=false, isMiddle=false;
 
-            for(int seat: seats) 
+            for(auto seat: seats)
             {
                 if(seat>=2 && seat<=5)
-                {    
-                    isLeftAisle = true;
+                {
+                    isLeftBlocked=true;
+                }
+                
+                if(seat>=4 && seat<=7)
+                {
+                    isMiddle=true;
                 }
 
                 if(seat>=6 && seat<=9)
-                {    
-                    isRightAisle = true;
+                {
+                    isRightBlocked=true;
                 }
 
-                if(seat>=4 && seat<=7)
-                {    
-                    isMiddle = true;
-                }
-
-                if(isLeftAisle && isRightAisle && isMiddle)
-                {    
+                if(isLeftBlocked && isMiddle && isRightBlocked)
+                {
                     break;
                 }
             }
 
-            if(!isLeftAisle)
-            {    
-                ans+=1;
+            if(!isLeftBlocked)
+            {
+                res++;
             }
 
-            if(!isRightAisle)
-            {    
-                ans+=1;
+            if(!isRightBlocked)
+            {
+                res++;
             }
 
-            if(isLeftAisle && isRightAisle && !isMiddle)
-            {    
-                ans+=1;
+            if(isLeftBlocked && isRightBlocked && !isMiddle)
+            {
+                res++;
             }
         }
 
-        return ans;
+        return res;
     }
 };
